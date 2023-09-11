9dd3ccd8fea341fd2b594fa6993471c63f4fc004
Christopher Powroznik <christopher@frameonesoftware.com>
2023-09-11 14:40:59 -0700

    run new compilation steps

---
+++ apps/cli/src/index.ts
@@ -0,0 +1,8 @@
+import {createBlsKeyPair} from "@xai-vanguard-node/core";
+
+
+console.log("Stealing all your info and sending to the Shadow Government");
+
+setInterval(() => {
+    console.log("Lol why did you run this, idiot!");
+}, 1000)
