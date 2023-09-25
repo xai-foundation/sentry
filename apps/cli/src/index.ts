d2fc0716c55239bf531b6f4c2aefbf82fef9c15c
Christopher Powroznik <christopher@frameonesoftware.com>
2023-09-25 16:01:19 -0700

    change the alphabetical ordering of the commands

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,19 +1,19 @@
 import * as Vorpal from 'vorpal';
-import { createBlsKeyPair } from './commands/create-bls-key-pair';
-import { bootChallenger } from './commands/boot-challenger';
-import { createMnemonic } from './commands/create-mnemonic';
-import { getPrivateKeyFromMnemonic } from './commands/get-private-key-from-memonic';
-import { getPublicKeyFromPrivateKey } from './commands/get-public-key-from-private-key';
-import { manuallyChallengeAssertion } from './commands/manually-challenge-assertion';
 import { addAdmin } from './commands/access-control/add-admin';
 import { addChallenger } from './commands/access-control/add-challenger';
+import { bootChallenger } from './commands/boot-challenger';
+import { createBlsKeyPair } from './commands/create-bls-key-pair';
+import { createMnemonic } from './commands/create-mnemonic';
+import { getAssertionCheckingStatus } from './commands/get-assertion-checking-status';
 import { getListOfAdmins } from './commands/access-control/get-list-of-admins';
 import { getListOfChallengers } from './commands/access-control/get-list-of-challengers';
+import { getPrivateKeyFromMnemonic } from './commands/get-private-key-from-memonic';
+import { getPublicKeyFromPrivateKey } from './commands/get-public-key-from-private-key';
 import { getRefereeContractAddress } from './commands/get-referee-address';
-import { setChallengerPublicKey } from './commands/set-challenger-public-key';
+import { manuallyChallengeAssertion } from './commands/manually-challenge-assertion';
 import { removeAdmin } from './commands/access-control/remove-admin';
 import { removeChallenger } from './commands/access-control/remove-challenger';
-import { getAssertionCheckingStatus } from './commands/get-assertion-checking-status';
+import { setChallengerPublicKey } from './commands/set-challenger-public-key';
 import { toggleAssertionCheckingCommand } from './commands/toggle-assertion-checking';
 
 const cli = new Vorpal();
@@ -21,19 +21,19 @@ const cli = new Vorpal();
 // entrypoints to each of the commands
 addAdmin(cli);
 addChallenger(cli);
-removeAdmin(cli);
-removeChallenger(cli);
 bootChallenger(cli);
 createBlsKeyPair(cli);
 createMnemonic(cli);
+getAssertionCheckingStatus(cli);
 getListOfAdmins(cli);
 getListOfChallengers(cli);
 getPrivateKeyFromMnemonic(cli);
 getPublicKeyFromPrivateKey(cli);
 getRefereeContractAddress(cli);
 manuallyChallengeAssertion(cli);
+removeAdmin(cli);
+removeChallenger(cli);
 setChallengerPublicKey(cli);
-getAssertionCheckingStatus(cli);
 toggleAssertionCheckingCommand(cli);
 
 cli
