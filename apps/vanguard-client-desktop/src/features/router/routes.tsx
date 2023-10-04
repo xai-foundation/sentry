17c064c7271fccd1643cf83ba66270715dee7c97
Christopher Powroznik <christopher@frameonesoftware.com>
2023-10-04 16:13:35 -0700

    swap out for hash router

--- apps/vanguard-client-desktop/src/features/router/routes.tsx
+++ apps/vanguard-client-desktop/src/features/router/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Licenses } from "../licenses/Licenses.tsx";
 import { Homepage } from "../home/Homepage.tsx";
 import Sidebar from "../sidebar/SidebarRoot";
