82ebc1035e2dcd42366c7f44719a7761c6a3aa4a
Christopher Powroznik <christopher@frameonesoftware.com>
2023-10-09 18:50:22 -0700

    deploy new contracts

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -2,7 +2,7 @@ export const config = {
   "arbitrumBlockExplorer": "https://arbiscan.io",
   "arbitrumGoerliBlockExplorer": "https://goerli.arbiscan.io",
   "defaultRpcUrl": "https://goerli-rollup.arbitrum.io/rpc",
-  "nodeLicenseAddress": "0xef2174b8B6a5B822113A2362BA60926c0331a50B",
-  "refereeAddress": "0x9E8B904A241388022d92C0054E21F0d2f6f178aF",
+  "nodeLicenseAddress": "0x8Cb6bAF6337d64b58baA3A249e43906021D7db15",
+  "refereeAddress": "0x6979161595d711842864a1d9f2327e1418063907",
   "rollupAddress": "0x082742561295f6e1b43c4f5d1e2d52d7FfE082f1"
 };
