62341967fa971ffd305f8bc3cadc733a3171267b
Christopher Powroznik <christopher@expopulus.com>
2023-10-11 10:44:44 -0700

    update deployment addresses

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -2,9 +2,9 @@ export const config = {
   "arbitrumBlockExplorer": "https://arbiscan.io",
   "arbitrumGoerliBlockExplorer": "https://goerli.arbiscan.io",
   "defaultRpcUrl": "https://goerli-rollup.arbitrum.io/rpc",
-  "esXaiAddress": "0x418Fb733e3B90e35d256f3f0f6B8fA312559E900",
-  "nodeLicenseAddress": "0x803DceA68Ad4263B697dFd5A2D27D372d5937f63",
-  "refereeAddress": "0xe223966B6Ed1209c83b879daf3a42bD6F726A717",
+  "esXaiAddress": "0x79Bf74A93D86dce235471751a7d359c65bF67862",
+  "nodeLicenseAddress": "0x465EEF81240A8620e645660678a9E0304C606c6f",
+  "refereeAddress": "0xB934633A8A082c1c9b6172eF2888b8A5fD10E556",
   "rollupAddress": "0x082742561295f6e1b43c4f5d1e2d52d7FfE082f1",
-  "xaiAddress": "0x32D15bDe04e03468F99264ce528C096Af56FdA63"
+  "xaiAddress": "0xEF8BcCC86E4864ccEc6EBdA0f14279f2C5535B5d"
 };
