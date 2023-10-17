9f3c7ac47b9e2d99f2956619dcd3978ca8f96f21
Christopher Powroznik <christopher@frameonesoftware.com>
2023-10-17 14:07:04 -0700

    mono repo setup for ui library

---
+++ apps/vanguard-client-desktop/vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import electron from 'vite-plugin-electron/simple';
+import react from '@vitejs/plugin-react';
+import tailwindcss from "tailwindcss";
+import autoprefixer from 'autoprefixer';
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            'crypto': 'crypto-js',
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss,
+                autoprefixer,
+            ],
+        },
+    },
+    plugins: [
+        react(),
+        electron({
+            main: {
+                // Shortcut of `build.lib.entry`.
+                entry: 'electron/main.ts',
+            },
+            preload: {
+                // Shortcut of `build.rollupOptions.input`.
+                // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+                input: path.join(__dirname, 'electron/preload.ts'),
+            },
+            // Ployfill the Electron and Node.js built-in modules for Renderer process.
+            // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+            renderer: {},
+        }),
+    ],
+});
