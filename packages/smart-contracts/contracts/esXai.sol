009612c255e4fa0fc73f4ce99b0d1cd0623aad14
Christopher Powroznik <christopher@frameonesoftware.com>
2023-10-18 14:46:03 -0700

    made esxai upgradeable before deploy script

--- packages/smart-contracts/contracts/esXai.sol
+++ packages/smart-contracts/contracts/esXai.sol
@@ -2,25 +2,29 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
 import "./Xai.sol";
 
 /**
  * @title esXai
  * @dev Implementation of the esXai
  */
-contract esXai is ERC20, ERC20Burnable, AccessControl {
+contract esXai is ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Xai private _xai;
-    using EnumerableSet for EnumerableSet.AddressSet;
-    EnumerableSet.AddressSet private _whitelist;
+    using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
+    EnumerableSetUpgradeable.AddressSet private _whitelist;
 
     event WhitelistUpdated(address account, bool isAdded);
 
-    constructor (Xai xai) ERC20("esXai", "esXAI") {
+    function initialize (Xai xai) public initializer {
+        __ERC20_init("esXai", "esXAI");
+        __ERC20Burnable_init();
+        __AccessControl_init();
+
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setRoleAdmin(MINTER_ROLE, DEFAULT_ADMIN_ROLE);
         _xai = xai;
