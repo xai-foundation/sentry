92d849c1a00303bcc94ce2e475d267651fa4dc83
James Bartnik <james@expopulus.com>
2023-10-24 14:59:45 -0700

    Adds load state to BuyKeysFlow

--- apps/vanguard-client-desktop/src/features/keys/modals/buy-keys/BuyKeysFlow.tsx
+++ apps/vanguard-client-desktop/src/features/keys/modals/buy-keys/BuyKeysFlow.tsx
@@ -1,7 +1,7 @@
 import {Dispatch, SetStateAction, useState} from "react";
 import {XaiNumberInput} from "@xai-vanguard-node/ui";
 import {ReactComponent as XaiLogo} from "@/svgs/xai-logo.svg";
-import {BiLinkExternal} from "react-icons/bi";
+import {BiLinkExternal, BiLoaderAlt} from "react-icons/bi";
 import {AiOutlineClose, AiOutlineInfoCircle} from "react-icons/ai";
 import {MdVerifiedUser} from "react-icons/md";
 
@@ -19,15 +19,17 @@ interface BuyKeysFlowProps {
 
 export function BuyKeysFlow({setPurchaseSuccess}: BuyKeysFlowProps) {
 	const [amount, setAmount] = useState<number>(1);
-	const [promo, setPromo] = useState<boolean>(false);
-	const [inputValue, setInputValue] = useState('');
-
 	const [discountApplied, setDiscountApplied] = useState<boolean>(false);
 	const [discountError, setDiscountError] = useState<boolean>(false);
+	const [inputValue, setInputValue] = useState('');
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [promo, setPromo] = useState<boolean>(false);
 
-	// Calculate 5% of the number
+
+	// todo: delete once real data is implemented
+	console.log(setIsLoading);
+
 	const discountPrice = (5 / 100) * payWithBody[0].price;
-
 	const discount = {
 		item: "Discount (5%)",
 		abbr: "ETH",
@@ -113,108 +115,120 @@ export function BuyKeysFlow({setPurchaseSuccess}: BuyKeysFlowProps) {
 			</div>
 
 			{/*		Order Total section		*/}
-			<div className="w-full flex flex-col gap-4">
-				<div className="w-full items-center gap-2">
-					<div className="w-full">
-						<hr/>
+			{isLoading
+				? (
+					<div className="w-full absolute top-0 bottom-0 flex flex-col justify-center items-center">
+						<BiLoaderAlt className="animate-spin" color={"#A3A3A3"} size={32}/>
+						<p>Updating total...</p>
 					</div>
-					<div className="w-12 flex flex-row text-sm uppercase text-[#A3A3A3] mt-4 px-6">
-						<p className="flex items-center gap-1">
-							Total
-							<AiOutlineInfoCircle size={16}/>
-						</p>
-					</div>
-				</div>
-
-				<div className="px-6">
-					{getOrderTotal()}
-					<p className="text-[13px] text-[#A3A3A3] mb-4">
-						{payWithBody[0].price} {payWithBody[0].abbr} per key
-					</p>
-
-					{discountApplied && (
-						<>
-							{getDiscount()}
-							<p className="text-[13px] text-[#A3A3A3] mb-4">
-								IDONTWANNAPAYFULLPRICE
-							</p>
-						</>
-					)}
-
-					{/*		Promo section		*/}
-					{!discountApplied && (
-						<>
-							<hr className="my-2"/>
-							{promo ? (
-									<div>
-										<div
-											className="w-full h-auto flex flex-row justify-between items-center text-[15px] text-[#525252] mt-2 py-2">
-											<span>Add promo code</span>
-											<div className="cursor-pointer z-10" onClick={() => setPromo(false)}>
-												<AiOutlineClose/>
-											</div>
-										</div>
-
-										<div className="flex gap-2 items-center">
-
-											<input
-												type="text"
-												value={inputValue}
-												onChange={(e) => {
-													setInputValue(e.target.value)
-													setDiscountError(false);
-												}}
-												className={`w-full my-2 p-2 border ${discountError ? "border-[#AB0914]" : "border-[#A3A3A3]"}`}
-												placeholder="Enter promo code"
-											/>
-
-											<button
-												onClick={() => handleSubmit()}
-												className="flex flex-row justify-center items-center w-[92px] p-2 bg-[#F30919] text-[15px] text-white font-semibold uppercase"
-											>
-												Apply
-											</button>
-										</div>
-
-										{discountError && (
-											<p className="text-[14px] text-[#AB0914]">Invalid referral address</p>
-										)}
-									</div>
-								) : (
-									<p className="text-[15px] py-2">
-										<a
-											onClick={() => setPromo(true)}
-											className="text-[#F30919] ml-1 cursor-pointer"
-										>
-											+ Add promo code
-										</a>
+				) : (
+					<>
+						<div className="w-full flex flex-col gap-4">
+							<div className="w-full items-center gap-2">
+								<div className="w-full">
+									<hr/>
+								</div>
+								<div className="w-12 flex flex-row text-sm uppercase text-[#A3A3A3] mt-4 px-6">
+									<p className="flex items-center gap-1">
+										Total
+										<AiOutlineInfoCircle size={16}/>
 									</p>
-								)
-							}
-						</>
-					)}
+								</div>
+							</div>
 
-					<hr className="my-2"/>
-					<div className="flex flex-row items-center justify-between">
-						<div className="flex flex-row items-center gap-2 text-lg">
-							<span className="">You pay</span>
-						</div>
-						<div className="flex flex-row items-center gap-1 font-semibold">
+							<div className="px-6">
+								{getOrderTotal()}
+								<p className="text-[13px] text-[#A3A3A3] mb-4">
+									{payWithBody[0].price} {payWithBody[0].abbr} per key
+								</p>
+
+								{discountApplied && (
+									<>
+										{getDiscount()}
+										<p className="text-[13px] text-[#A3A3A3] mb-4">
+											IDONTWANNAPAYFULLPRICE
+										</p>
+									</>
+								)}
+
+								{/*		Promo section		*/}
+								{!discountApplied && (
+									<>
+										<hr className="my-2"/>
+										{promo ? (
+											<div>
+												<div
+													className="w-full h-auto flex flex-row justify-between items-center text-[15px] text-[#525252] mt-2 py-2">
+													<span>Add promo code</span>
+													<div className="cursor-pointer z-10"
+														 onClick={() => setPromo(false)}>
+														<AiOutlineClose/>
+													</div>
+												</div>
+
+												<div className="flex gap-2 items-center">
+
+													<input
+														type="text"
+														value={inputValue}
+														onChange={(e) => {
+															setInputValue(e.target.value)
+															setDiscountError(false);
+														}}
+														className={`w-full my-2 p-2 border ${discountError ? "border-[#AB0914]" : "border-[#A3A3A3]"}`}
+														placeholder="Enter promo code"
+													/>
+
+													<button
+														onClick={() => handleSubmit()}
+														className="flex flex-row justify-center items-center w-[92px] p-2 bg-[#F30919] text-[15px] text-white font-semibold uppercase"
+													>
+														Apply
+													</button>
+												</div>
+
+												{discountError && (
+													<p className="text-[14px] text-[#AB0914]">Invalid referral
+														address</p>
+												)}
+											</div>
+										) : (
+											<p className="text-[15px] py-2">
+												<a
+													onClick={() => setPromo(true)}
+													className="text-[#F30919] ml-1 cursor-pointer"
+												>
+													+ Add promo code
+												</a>
+											</p>
+										)
+										}
+									</>
+								)}
+
+								<hr className="my-2"/>
+								<div className="flex flex-row items-center justify-between">
+									<div className="flex flex-row items-center gap-2 text-lg">
+										<span className="">You pay</span>
+									</div>
+									<div className="flex flex-row items-center gap-1 font-semibold">
 							<span>
 								{discountApplied
 									? Number((payWithBody[0].price * amount) - (discountPrice * amount)).toFixed(8)
 									: Number(payWithBody[0].price * amount).toFixed(8)}
 							</span>
-							<span>
+										<span>
 								{payWithBody[0].abbr}
 							</span>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
+					</>
+				)}
 
 			{/*		Banner section		*/}
-			<div className="w-full flex flex-col gap-4 px-6">
+			<div className="absolute bottom-0 w-full flex flex-col gap-4 px-6">
 				<div className="flex flex-col gap-2 bg-[#DCFCE6] p-6">
 					<span className="flex flex-row gap-1 items-center font-semibold">
 						<MdVerifiedUser size={22} color={"#38A349"}/> Purchase will be completed on <p
