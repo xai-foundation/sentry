7c3b291e1208e4d3a2537bc55cda140e9b673b8c
James Bartnik <james@expopulus.com>
2023-11-07 17:10:50 -0800

    Implements ImportSentryModal component

--- apps/vanguard-client-desktop/src/features/demo/Demo.tsx
+++ apps/vanguard-client-desktop/src/features/demo/Demo.tsx
@@ -1,19 +1,30 @@
 import {useState} from "react";
 import {Blockpass} from "../../components/blockpass/Blockpass.js";
 import {PurchaseCompleteModal} from "../home/modals/PurchaseCompleteModal.js";
+import {RemoveWalletModal} from "@/features/home/modals/RemoveWalletModal";
+import {ImportSentryAlertModal} from "@/features/home/modals/ImportSentryAlertModal";
 
 export function Demo() {
 	// const [showConnectedModal, setShowConnectedModal] = useState<boolean>(false);
 	const [showPurchaseCompleteModal, setShowPurchaseCompleteModal] = useState<boolean>(false);
+	const [showRemoveWalletModal, setShowRemoveWalletModal] = useState<boolean>(false);
+	const [showImportSentryAlertModal, setShowImportSentryAlertModal] = useState<boolean>(false);
 
 	return (
 		<div className="h-full flex justify-center items-center gap-4">
-			{/*<button*/}
-			{/*	onClick={() => setShowConnectedModal(true)}*/}
-			{/*	className="w-auto h-auto bg-[#F30919] text-white p-4 uppercase font-semibold"*/}
-			{/*>*/}
-			{/*	Open Wallet Connected Modal*/}
-			{/*</button>*/}
+			<button
+				onClick={() => setShowImportSentryAlertModal(true)}
+				className="w-auto h-auto bg-[#F30919] text-white p-4 uppercase font-semibold"
+			>
+				Open Import Sentry Alert Modal
+			</button>
+
+			<button
+				onClick={() => setShowRemoveWalletModal(true)}
+				className="w-auto h-auto bg-[#F30919] text-white p-4 uppercase font-semibold"
+			>
+				Open Remove Wallet Modal
+			</button>
 
 			<button
 				onClick={() => setShowPurchaseCompleteModal(true)}
@@ -24,11 +35,19 @@ export function Demo() {
 
 			<Blockpass/>
 
-			{/*{showConnectedModal && (*/}
-			{/*	<WalletConnectedModal*/}
-			{/*		setShowConnectedModal={setShowConnectedModal}*/}
-			{/*	/>*/}
-			{/*)}*/}
+			{showImportSentryAlertModal && (
+				<ImportSentryAlertModal
+					setShowModal={setShowImportSentryAlertModal}
+					onSuccess={() => alert("lol")}
+				/>
+			)}
+
+			{showRemoveWalletModal && (
+				<RemoveWalletModal
+					setShowModal={setShowRemoveWalletModal}
+					onSuccess={() => alert("lol")}
+				/>
+			)}
 
 			{showPurchaseCompleteModal && (
 				<PurchaseCompleteModal
---
+++ apps/vanguard-client-desktop/src/features/home/modals/RemoveWalletModal.tsx
@@ -0,0 +1,52 @@
+import {AiFillWarning, AiOutlineClose} from "react-icons/ai";
+import {Dispatch, SetStateAction} from "react";
+
+interface ImportSentryAlertModalProps {
+	setShowModal: Dispatch<SetStateAction<boolean>>;
+	onSuccess: () => void;
+}
+
+export function RemoveWalletModal({setShowModal, onSuccess}: ImportSentryAlertModalProps) {
+
+
+	return (
+		<div
+			className="absolute top-0 right-0 left-0 bottom-0 m-auto w-auto h-auto flex flex-col justify-start items-center z-30">
+			<div className="w-full h-full bg-white opacity-75"/>
+			<div
+				className="absolute top-0 right-0 left-0 bottom-0 m-auto flex flex-col justify-start items-center w-[506px] h-[272px] border border-gray-200 bg-white">
+				<div
+					className="absolute top-0 right-0 h-16 flex flex-row justify-between items-center text-lg px-6">
+					<div className="cursor-pointer z-10" onClick={() => setShowModal(false)}>
+						<AiOutlineClose/>
+					</div>
+				</div>
+				<div className="w-full h-full flex flex-col justify-center items-center gap-2">
+					<AiFillWarning className="w-16 h-16 text-[#F59E28]"/>
+					<p className="text-[15px] font-semibold">
+						Are you sure you want to remove this wallet?
+					</p>
+					<p className="text-[#525252] text-[15px]">
+						You will stop tracking all esXAI accrued on this wallet
+					</p>
+
+					<div className="flex gap-8 my-4">
+						<button
+							onClick={() => onSuccess()}
+							className="w-fit h-auto text-[15px] text-[#F30919] px-4 py-3 font-semibold"
+						>
+							Yes, remove this wallet
+						</button>
+						<button
+							onClick={() => setShowModal(false)}
+							className="w-fit h-auto bg-[#F30919] text-[15px] text-white px-4 py-3 font-semibold"
+						>
+							No, take me back
+						</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	)
+}
