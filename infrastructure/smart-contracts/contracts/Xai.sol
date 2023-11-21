6b64e37aa006067ba55533910363b1fdf557cb47
Christopher Powroznik <christopher@frameonesoftware.com>
2023-11-21 14:51:00 -0800

    before testnet deploy

--- infrastructure/smart-contracts/contracts/Xai.sol
+++ infrastructure/smart-contracts/contracts/Xai.sol
@@ -15,7 +15,7 @@ contract Xai is ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgrade
     uint256 public constant MAX_SUPPLY = 2500000000 * 10**18; // Max supply of 2,500,000,000 tokens
 
     function initialize() public initializer {
-        __ERC20_init("Xai", "XAI");
+        __ERC20_init("SXai", "SXAI");
         __ERC20Burnable_init();
         __AccessControl_init();
 
@@ -30,7 +30,7 @@ contract Xai is ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgrade
      * @return A boolean that indicates if the operation was successful.
      */
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) returns (bool) {
-        require(totalSupply() + amount <= MAX_SUPPLY, "Cannot mint beyond max supply");
+        // require(totalSupply() + amount <= MAX_SUPPLY, "Cannot mint beyond max supply"); // not needed for testnet
         _mint(to, amount);
         return true;
     }
