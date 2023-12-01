acac2eb3d632ca4ab0b7f7e63c287462688c4157
James Bartnik <james@expopulus.com>
2023-12-01 15:20:50 -0800

    Updates buy flow checkbox copy

--- apps/web-connect/src/features/checkout/WebBuyKeysOrderTotal.tsx
+++ apps/web-connect/src/features/checkout/WebBuyKeysOrderTotal.tsx
@@ -37,9 +37,10 @@ export function WebBuyKeysOrderTotal(
 	const {isLoading: isTotalLoading} = useGetTotalSupplyAndCap();
 
 	const [promo, setPromo] = useState<boolean>(false);
-	const [terms, setTerms] = useState<boolean>(false);
-	const [investments, setInvestments] = useState<boolean>(false);
-	const ready = terms && investments;
+	const [checkboxOne, setCheckboxOne] = useState<boolean>(false);
+	const [checkboxTwo, setCheckboxTwo] = useState<boolean>(false);
+	const [checkboxThree, setCheckboxThree] = useState<boolean>(false);
+	const ready = checkboxOne && checkboxTwo && checkboxThree;
 
 	const handleSubmit = async () => {
 		const validatePromoCode = await getPromoCode(promoCode);
@@ -226,41 +227,37 @@ export function WebBuyKeysOrderTotal(
 						<div className="flex flex-col justify-center gap-8 p-6 mt-8">
 							<div className="flex flex-col justify-center gap-2">
 								<XaiCheckbox
-									onClick={() => setTerms(!terms)}
-									condition={terms}
+									onClick={() => setCheckboxOne(!checkboxOne)}
+									condition={checkboxOne}
 								>
-									I agree to the
+									I agree with the
 									<a
 										className="cursor-pointer text-[#F30919]"
-										onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}>
-										Terms of Service
-									</a>
-									and the
-									<a
-										className="cursor-pointer text-[#F30919]"
-										onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}>
-										Privacy Policy
+										onClick={() => window.open("https://xai.games/sentrynodeagreement/")}>
+										Sentry Node Agreement
 									</a>
 								</XaiCheckbox>
 
 
 								<XaiCheckbox
-									onClick={() => setInvestments(!investments)}
-									condition={investments}
+									onClick={() => setCheckboxTwo(!checkboxTwo)}
+									condition={checkboxTwo}
 								>
-									I understand that Xai Vanguard Nodes are
-									<a
-										className="cursor-pointer text-[#F30919]"
-										onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}>
-										not investments
-									</a>
+									I understand Sentry Node Keys are not transferable
+								</XaiCheckbox>
+
+								<XaiCheckbox
+									onClick={() => setCheckboxThree(!checkboxThree)}
+									condition={checkboxThree}
+								>
+									I understand that I cannot claim rewards until I pass KYC
 								</XaiCheckbox>
 							</div>
 
 							<div>
 								<button
 									onClick={() => onClick()}
-									className={`w-full h-16 ${investments && terms ? "bg-[#F30919]" : "bg-gray-400 cursor-default"} text-sm text-white p-2 uppercase font-semibold`}
+									className={`w-full h-16 ${checkboxOne && checkboxTwo && checkboxThree ? "bg-[#F30919]" : "bg-gray-400 cursor-default"} text-sm text-white p-2 uppercase font-semibold`}
 									disabled={!ready}
 								>
 									Confirm purchase
