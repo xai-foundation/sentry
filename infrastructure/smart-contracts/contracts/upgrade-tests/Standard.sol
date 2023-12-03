39f0ee33b3d624ec34e4476c5c8a50bd79298ad5
Christopher Powroznik <christopher@expopulus.com>
2023-12-03 14:47:49 -0800

    Test contract for testing access control

---
+++ infrastructure/smart-contracts/contracts/upgrade-tests/Standard.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract CounterContract is AccessControlUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    bytes32 public constant COUNTER_ROLE = keccak256("COUNTER_ROLE");
+    CountersUpgradeable.Counter private _count;
+
+    function initialize() public initializer {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(COUNTER_ROLE, _msgSender());
+    }
+
+    function incrementCount() public {
+        require(hasRole(COUNTER_ROLE, _msgSender()), "CounterContract: must have counter role to increment");
+        _count.increment();
+    }
+
+    function getCount() public view returns (uint256) {
+        return _count.current();
+    }
+}
