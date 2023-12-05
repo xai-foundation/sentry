e9ca8cbf345b39c80ebb82fbd2753ff51252865b
Victor Li <victor@expopulus.com>
2023-12-04 19:37:46 -0800

    test update again

--- apps/sentry-client-desktop/src/features/home/GetSentryNode.tsx
+++ apps/sentry-client-desktop/src/features/home/GetSentryNode.tsx
@@ -53,7 +53,7 @@ export function GetSentryNode() {
 			<div className="flex flex-col">
 				<div className="flex gap-1 items-center">
 					<h1 className="text-[40px] font-bold uppercase tracking-widest">
-						Get a Xai Sentry Node 0.1.172
+						Get a Xai Sentry Node 0.1.173
 					</h1>
 					<GetSentryNodeTooltip
 						width={436}
