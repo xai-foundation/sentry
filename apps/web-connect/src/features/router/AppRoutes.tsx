88b5e285e019638eeff66e7c13d2c05c152f5c1e
James Bartnik <james@expopulus.com>
2023-12-06 11:57:25 -0800

    Geo-fencing

--- apps/web-connect/src/features/router/AppRoutes.tsx
+++ apps/web-connect/src/features/router/AppRoutes.tsx
@@ -5,22 +5,90 @@ import {ConnectWallet} from "../wallet/routes/ConnectWallet.js";
 import {AssignWallet} from "../wallet/routes/AssignWallet.js";
 import {UnassignWallet} from "@/features/wallet/routes/UnassignWallet";
 import {Header} from "@/features/header/Header";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {BiLoaderAlt} from "react-icons/bi";
+
+enum IpBanType {
+	INVALID_IP = "INVALID_IP",
+	GEO = "GEO",
+}
+
+enum GeoBanType {
+	OFAC_SANCTIONS = "OFAC_SANCTIONS",
+	GAMBLING = "GAMBLING",
+	SECURITIES = "SECURITIES",
+}
+
+interface checkIpProps {
+	type: IpBanType;
+	geoBanType: GeoBanType;
+}
 
 export function AppRoutes() {
 	const queryClient = new QueryClient();
+	const [blocked, setBlocked] = useState(true)
+	const [loading, setLoading] = useState(true)
 
-	return (
-		<Router basename={"/"}>
-			<QueryClientProvider client={queryClient}>
-				<Header/>
-				<Routes>
-					<Route path="/" element={<Checkout/>}/>
-					<Route path="/connect-wallet" element={<ConnectWallet/>}/>
-					<Route path="/assign-wallet/:operatorAddress" element={<AssignWallet/>}/>
-					<Route path="/unassign-wallet/:operatorAddress" element={<UnassignWallet/>}/>
-					<Route path="*" element={<Navigate to="/" replace={true}/>}/>
-				</Routes>
-			</QueryClientProvider>
-		</Router>
-	);
+	useEffect(() => {
+		console.log("running");
+		void checkIp();
+	}, [location.pathname]);
+
+	async function checkIp() {
+		try {
+			const {data} = await axios.post(`https://centralized-services.expopulus.com/check-ip`);
+
+			console.log("data", data);
+
+			const invalidIp = data.reasons?.find(({type}: checkIpProps) => type === "INVALID_IP");
+			const ofacSanction = data.reasons?.find(({type, geoBanType}: checkIpProps) => type === "GEO" && geoBanType === "OFAC_SANCTIONS");
+
+			console.log("invalidIp", invalidIp)
+			console.log("ofacSanction", ofacSanction)
+
+			if (!invalidIp || !ofacSanction) {
+				setBlocked(false);
+				setLoading(false);
+			}
+
+			if (!!invalidIp || !!ofacSanction) {
+				setBlocked(true);
+				setLoading(false);
+			}
+		} catch (e: any) {
+			console.error(e.response.data);
+		}
+	}
+
+	if (loading) {
+		return (
+			<div className="w-full h-screen flex justify-center items-center">
+				<BiLoaderAlt className="animate-spin" size={32} color={"#000000"}/>
+			</div>
+		)
+	}
+
+	if (!loading && blocked) {
+		return (
+			<pre className="p-2 text-[14px]">Not Found</pre>
+		)
+	}
+
+	if (!loading && !blocked) {
+		return (
+			<Router basename={"/"}>
+				<QueryClientProvider client={queryClient}>
+					<Header/>
+					<Routes>
+						<Route path="/" element={<Checkout/>}/>
+						<Route path="/connect-wallet" element={<ConnectWallet/>}/>
+						<Route path="/assign-wallet/:operatorAddress" element={<AssignWallet/>}/>
+						<Route path="/unassign-wallet/:operatorAddress" element={<UnassignWallet/>}/>
+						<Route path="*" element={<Navigate to="/" replace={true}/>}/>
+					</Routes>
+				</QueryClientProvider>
+			</Router>
+		);
+	}
 }
