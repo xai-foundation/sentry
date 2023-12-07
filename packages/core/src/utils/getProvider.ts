d142585f175468e680e0394d4645d8fb83ea817d
Christopher Powroznik <christopher@expopulus.com>
2023-12-06 20:19:59 -0800

    make a random url get selected

--- packages/core/src/utils/getProvider.ts
+++ packages/core/src/utils/getProvider.ts
@@ -11,7 +11,12 @@ const providers: { [url: string]: ethers.JsonRpcProvider } = {};
  * @returns An ethers provider.
  */
 export function getProvider(_rpcUrl?: string): ethers.JsonRpcProvider {
-    const rpcUrl = _rpcUrl ? _rpcUrl :  config.defaultRpcUrl;
+    const providerUrls = [
+        "https://arb-mainnet.g.alchemy.com/v2/p_LSgTIj_JtEt3JPM7IZIZFL1a70yvQJ",
+        // "https://arb1.arbitrum.io/rpc",
+        "https://tame-alpha-violet.arbitrum-mainnet.quiknode.pro/d55a31b32f04c82b0e1bcb77f1fc6dcf53147f2a/"
+    ];
+    const rpcUrl = _rpcUrl ? _rpcUrl : providerUrls[Math.floor(Math.random() * providerUrls.length)];
     if (!providers[rpcUrl]) {
         providers[rpcUrl] = new ethers.JsonRpcProvider(rpcUrl);
     }
