3c87562c966d9e48563a2f1be18142ff9333c84b
Christopher Powroznik <christopher@expopulus.com>
2023-12-08 15:49:12 -0800

    implement filter refreshing

--- packages/core/src/utils/getProvider.ts
+++ packages/core/src/utils/getProvider.ts
@@ -4,15 +4,20 @@ import { ethers } from 'ethers';
 const providers: { [url: string]: ethers.JsonRpcProvider } = {};
 
 /**
- * Creates an ethers provider from a given RPC URL. If the same RPC URL is passed in, 
+ * Creates an ethers provider from a given RPC URL. If the same RPC URL is passed in and ignoreMemo is false, 
  * the same instance of the provider is returned (Singleton nature).
  * @param rpcUrl - The RPC URL. Defaults to Arbitrum One's public RPC.
+ * @param ignoreMemo - A flag to ignore the memo. Defaults to false.
  * @returns An ethers provider.
  */
-export function getProvider(_rpcUrl: string = "https://arb-mainnet.g.alchemy.com/v2/p_LSgTIj_JtEt3JPM7IZIZFL1a70yvQJ"): ethers.JsonRpcProvider {
+export function getProvider(_rpcUrl: string = "https://arb-mainnet.g.alchemy.com/v2/p_LSgTIj_JtEt3JPM7IZIZFL1a70yvQJ", ignoreMemo: boolean = false): ethers.JsonRpcProvider {
     const rpcUrl = _rpcUrl;
-    if (!providers[rpcUrl]) {
-        providers[rpcUrl] = new ethers.JsonRpcProvider(rpcUrl);
+    if (ignoreMemo || !providers[rpcUrl]) {
+        const provider = new ethers.JsonRpcProvider(rpcUrl);
+        if (!ignoreMemo) {
+            providers[rpcUrl] = provider;
+        }
+        return provider;
     }
     return providers[rpcUrl];
 }
