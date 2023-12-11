1f3a29f389a6c22f25d1c3468d2ec4ada51e2763
James Bartnik <james@expopulus.com>
2023-12-11 14:04:55 -0800

    WIP: Implement React Sentry

--- apps/sentry-client-desktop/src/features/router/routes.tsx
+++ apps/sentry-client-desktop/src/features/router/routes.tsx
@@ -11,28 +11,11 @@ import {ChainDataManager} from "@/components/ChainDataManager";
 import {createStore, Provider as JotaiProvider} from "jotai";
 import {AccruingDataManager} from "@/components/AccruingDataManager";
 import toast, {Toaster} from "react-hot-toast";
-import * as Sentry from "@sentry/react";
 
 const store = createStore();
 
 export function AppRoutes() {
 	const queryClient = new QueryClient();
-
-	Sentry.init({
-		dsn: "https://a72bd57d284f8711761b36655e40b65e@o4506378569777152.ingest.sentry.io/4506378571612160",
-		integrations: [
-			new Sentry.BrowserTracing({
-				// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-				tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-			}),
-			new Sentry.Replay(),
-		],
-		// Performance Monitoring
-		tracesSampleRate: 1.0, //  Capture 100% of the transactions
-		// Session Replay
-		replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-		replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-	});
 
 	window.ipcRenderer.on("update-available", () => {
 		if (window.electron.platform === "win32") {
--- apps/sentry-client-desktop/src/main.tsx
+++ apps/sentry-client-desktop/src/main.tsx
@@ -2,9 +2,27 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import {AppRoutes} from './features/router'
+import * as Sentry from "@sentry/react";
 import "./index.css";
 
 const rootElement = document.getElementById('root')!
+
+Sentry.init({
+	dsn: "https://a72bd57d284f8711761b36655e40b65e@o4506378569777152.ingest.sentry.io/4506378571612160",
+	integrations: [
+		new Sentry.BrowserTracing({
+			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+			tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+		}),
+		new Sentry.Replay(),
+	],
+	// Performance Monitoring
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+	// Session Replay
+	replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+	replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement)
 	root.render(
