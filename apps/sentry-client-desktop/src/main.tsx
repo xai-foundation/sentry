5d1e67fdd3fad90633933e37335d7499fc63279e
James Bartnik <james@expopulus.com>
2023-12-13 11:41:41 -0800

    Additional Sentry configuration changes

--- apps/sentry-client-desktop/src/main.tsx
+++ apps/sentry-client-desktop/src/main.tsx
@@ -3,6 +3,7 @@ import {StrictMode, useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
 import {AppRoutes} from './features/router'
 import * as Sentry from "@sentry/react";
+import {HttpClient} from "@sentry/integrations";
 import "./index.css";
 import {createRoutesFromChildren, matchRoutes, useLocation, useNavigationType} from "react-router-dom";
 
@@ -11,7 +12,6 @@ const rootElement = document.getElementById('root')!
 Sentry.init({
 	release: import.meta.env.APP_VERSION,
 	dsn: "https://a72bd57d284f8711761b36655e40b65e@o4506378569777152.ingest.sentry.io/4506378571612160",
-
 
 	integrations: [
 		new Sentry.BrowserTracing({
@@ -27,6 +27,8 @@ Sentry.init({
 		}),
 		// Following are true by default: maskAllText: true, blockAllMedia: true,
 		new Sentry.Replay(),
+
+		new HttpClient()
 	],
 	// Performance Monitoring
 	tracesSampleRate: 0.1, // Capture 10% of the transactions
@@ -35,6 +37,8 @@ Sentry.init({
 	// Session Replay
 	replaysSessionSampleRate: 0.1, // Sample rate = 10%.
 	replaysOnErrorSampleRate: 1.0, // Sample rate= 100% where errors occur.
+	
+	sendDefaultPii: true, // This option is required for capturing headers and cookies.
 });
 
 if (!rootElement.innerHTML) {
