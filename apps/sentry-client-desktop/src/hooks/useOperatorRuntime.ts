41fdafbcfb7060350fb152e4f601894e09465e77
James Bartnik <james@expopulus.com>
2023-12-21 09:18:49 -0800

    Puts whitelistedWallets back into useOperatorRuntime

--- apps/sentry-client-desktop/src/hooks/useOperatorRuntime.ts
+++ apps/sentry-client-desktop/src/hooks/useOperatorRuntime.ts
@@ -42,7 +42,7 @@ export function useOperatorRuntime() {
 			await setData({...data, sentryRunning: true});
 
 			// @ts-ignore
-			stop = await operatorRuntime(signer, setNodeLicenseStatusMap, writeLog);
+			stop = await operatorRuntime(signer, setNodeLicenseStatusMap, writeLog, whitelistedWallets);
 			setRerender((_number) => _number + 1);
 		}
 	}
@@ -56,7 +56,7 @@ export function useOperatorRuntime() {
 			await _stop();
 			setNodeLicenseStatusMap(new Map<bigint, NodeLicenseInformation>());
 			setSentryRunning(false);
-			setData({...data, sentryRunning: false});
+			await setData({...data, sentryRunning: false});
 		}
 	}
 
