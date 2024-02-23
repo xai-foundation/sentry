3e54d61501852177bbeee48ee7cea624387df8cc
spencer gray <spencer@expopulus.com>
2024-02-23 12:44:29 -0800

    add tests

--- infrastructure/smart-contracts/scripts/runCNYAirDrop.mjs
+++ infrastructure/smart-contracts/scripts/runCNYAirDrop.mjs
@@ -4,15 +4,11 @@ import fs from "fs/promises";
 import holders from "../assets/csv-output/xai-sentry-holders-cny.json" assert {type: "json"};
 import progress from "../assets/csv-output/cny-airdrop-progress.json" assert {type: "json"};
 
-async function runCNYAirDrop() {
+export async function runCNYAirDrop(deployer, fromAddress) {
 	const progressLocal = progress;
-
-	const [deployer] = (await ethers.getSigners());
-	const deployerAddress = await deployer.getAddress();
+	const totalHolders = Object.entries(holders).length;
 
 	const contract = new Contract(config.xaiAddress, XaiAbi);
-
-	const totalHolders = Object.entries(holders).length;
 
 	try {
 		for (const holder of holders) {
@@ -20,7 +16,7 @@ async function runCNYAirDrop() {
 				continue;
 			}
 
-			// await contract.connect(deployerAddress).transferFrom("TODO", holder.userWalletAddress, holder.amount);
+			await contract.connect(deployer).transferFrom(fromAddress, holder.userWalletAddress, holder.amount);
 
 			const myIndex = holders.findIndex(h => h.userWalletAddress === holder.userWalletAddress)
 			console.log(`Transferred ${holder.amount} to ${holder.userWalletAddress} (${myIndex + 1} of ${totalHolders}).`);
@@ -34,7 +30,9 @@ async function runCNYAirDrop() {
 	await fs.writeFile("./assets/csv-output/cny-airdrop-progress.json", JSON.stringify(progressLocal), {}, () => {});
 }
 
-runCNYAirDrop().then(() => {
+const [deployer] = (await ethers.getSigners());
+const fromAddress = "0xec57C2bF6B40F9eC0229c13be046087ABA59E9e1";
+runCNYAirDrop(deployer, fromAddress).then(() => {
 	console.log("Done CNY air drop!")
 }).catch((error) => {
 	console.error(error);
