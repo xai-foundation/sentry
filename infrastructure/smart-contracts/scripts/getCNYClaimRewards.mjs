0e3547096cad4bf50b54cfe60e97a12bc6a92f0f
spencer gray <spencer@expopulus.com>
2024-02-24 10:37:52 -0800

    update script to check kyc status

--- infrastructure/smart-contracts/scripts/getCNYClaimRewards.mjs
+++ infrastructure/smart-contracts/scripts/getCNYClaimRewards.mjs
@@ -1,15 +1,24 @@
 import csv from "csvtojson";
 import fs from "fs";
+import {Contract} from "ethers";
+import {config, RefereeAbi} from "@sentry/core";
 
 const wei = 1000000000000000000n;
 const baseReward = 80;
 const multipleReward = 8;
-async function getCNYClaimRewards() {
-	const holders = await csv().fromFile("../assets/csv-input/xai-sentry-holders-cny.csv");
+async function getCNYClaimRewards(deployer) {
+	const holders = await csv().fromFile("./assets/csv-input/xai-sentry-holders-cny.csv");
 
 	const rewards = new Map();
+	const refereeContract = new Contract(config.refereeAddress, RefereeAbi);
 
 	for (const holder of holders) {
+		const meKYC = await refereeContract.connect(deployer).isKycApproved(holder.HolderAddress);
+
+		if (!meKYC) {
+			continue;
+		}
+
 		const quantity = BigInt(baseReward + (holder.Quantity * multipleReward));
 		rewards.set(holder.HolderAddress, quantity * wei);
 	}
@@ -25,13 +34,14 @@ async function getCNYClaimRewards() {
 		total += BigInt(quantity);
 	}
 
-	await fs.writeFile("../assets/csv-output/xai-sentry-holders-cny.csv", walletXaiCsv, {}, () => {});
-	await fs.writeFile("../assets/csv-output/xai-sentry-holders-cny.json", JSON.stringify(json, null, 2), {}, () => {});
+	await fs.writeFile("./assets/csv-output/xai-sentry-holders-cny.csv", walletXaiCsv, {}, () => {});
+	await fs.writeFile("./assets/csv-output/xai-sentry-holders-cny.json", JSON.stringify(json, null, 2), {}, () => {});
 
 	console.log("total xai:", total);
 }
 
-getCNYClaimRewards().then(() => {
+const [deployer] = (await ethers.getSigners());
+getCNYClaimRewards(deployer).then(() => {
 	console.log("Done!")
 }).catch((error) => {
 	console.error(error);
