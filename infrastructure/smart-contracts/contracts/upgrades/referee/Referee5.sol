3a222ccfb25b975488ed9c37a8a79c9746cf4535
spencer gray <spencer@expopulus.com>
2024-03-08 14:28:23 -0800

    add / remove approval for pool owner on user stake/un-stake

--- infrastructure/smart-contracts/contracts/upgrades/referee/Referee5.sol
+++ infrastructure/smart-contracts/contracts/upgrades/referee/Referee5.sol
@@ -821,9 +821,9 @@ contract Referee5 is Initializable, AccessControlEnumerableUpgradeable {
         uint256 keysLength = keyIds.length;
         require(assignedKeysToPoolCount[pool] + keysLength <= maxKeysPerPool, "Maximum staking amount exceeded");
 
-		if (!isApprovedForOperator(keyOwner, poolOwner)) {
-			_operatorApprovals[keyOwner].add(poolOwner);
-			_ownersForOperator[poolOwner].add(keyOwner);
+		if (staker != poolOwner && !isApprovedForOperator(staker, poolOwner)) {
+			_operatorApprovals[staker].add(poolOwner);
+			_ownersForOperator[poolOwner].add(staker);
 		}
 
         NodeLicense nodeLicenseContract = NodeLicense(nodeLicenseAddress);
@@ -837,7 +837,7 @@ contract Referee5 is Initializable, AccessControlEnumerableUpgradeable {
         assignedKeysOfUserCount[staker] += keysLength;
     }
 
-    function unstakeKeys(address pool, address staker, address poolOwner, uint256[] memory keyIds) external onlyPoolFactory {
+    function unstakeKeys(address pool, address poolOwner, address staker, uint256[] memory keyIds, uint256 userStakedKeyCount) external onlyPoolFactory {
         uint256 keysLength = keyIds.length;
         NodeLicense nodeLicenseContract = NodeLicense(nodeLicenseAddress);
 
@@ -846,9 +846,12 @@ contract Referee5 is Initializable, AccessControlEnumerableUpgradeable {
                 assignedKeysOfUserCount[staker] > keysLength,
                 "Pool owner needs at least 1 staked key"
             );
-        }
-
-        //TODO remove approval
+        } else {
+			if (userStakedKeyCount - keysLength < 1) {
+				_operatorApprovals[staker].remove(poolOwner);
+				_ownersForOperator[poolOwner].remove(staker);
+			}
+		}
 
         for (uint256 i = 0; i < keysLength; i++) {
             uint256 keyId = keyIds[i];
