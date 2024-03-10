2921612af45916dcc343853c0ecb39eddb067326
CryptIT Austria <office@cryptit.at>
2024-03-10 05:26:54 +0100

    update deployment staking v2 script

--- infrastructure/smart-contracts/scripts/deployStakingV2.mjs
+++ infrastructure/smart-contracts/scripts/deployStakingV2.mjs
@@ -1,10 +1,7 @@
 import hardhat from "hardhat";
 const { ethers, upgrades } = hardhat;
-import { esXaiAbi } from "@sentry/core";
 import { safeVerify } from "../utils/safeVerify.mjs";
-
-const refereeAddress = "0xfD41041180571C5D371BEA3D9550E55653671198";
-const esXaiAddress = "0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c";
+import { esXaiAbi, config } from "@sentry/core";
 
 async function main() {
     const [deployer] = (await ethers.getSigners());
@@ -16,36 +13,58 @@ async function main() {
     const StakingPool = await ethers.deployContract("StakingPool");
     await StakingPool.waitForDeployment();
     const poolImplAddress = await StakingPool.getAddress();
+    console.log("Deployed Impl", poolImplAddress)
 
-    // //DEPLOY BUCKET TRACKER IMPL
+    // // //DEPLOY BUCKET TRACKER IMPL
     console.log("Deploying BucketTracker implementation...");
     const BucketTracker = await ethers.deployContract("BucketTracker");
     await BucketTracker.waitForDeployment();
     const bucketImplAddress = await BucketTracker.getAddress();
+    console.log("Deployed Impl", bucketImplAddress)
+
+    //ONLY NEEDED IN PROD DEPLOY FOR AUTO VERIFICATION FROM FACTORY CONTRACTS
+    console.log("Deploying TransparentUpgradeableProxyImplementationPool...");
+    const TransparentUpgradeableProxyImplementationPool = await ethers.deployContract("TransparentUpgradeableProxyImplementation", [poolImplAddress, deployerAddress, "0x"]);
+    await TransparentUpgradeableProxyImplementationPool.waitForDeployment();
+    const transparentUpgradeableProxyImplementationPool = await TransparentUpgradeableProxyImplementationPool.getAddress();
+    console.log("Deployed TransparentUpgradeableProxyImplementationPool", transparentUpgradeableProxyImplementationPool);
+
+    //ONLY NEEDED IN PROD DEPLOY FOR AUTO VERIFICATION FROM FACTORY CONTRACTS
+    console.log("Deploying transparentUpgradeableProxyImplementationBucket...");
+    const TransparentUpgradeableProxyImplementationBucket = await ethers.deployContract("TransparentUpgradeableProxyImplementation", [bucketImplAddress, deployerAddress, "0x"]);
+    await TransparentUpgradeableProxyImplementationBucket.waitForDeployment();
+    const transparentUpgradeableProxyImplementationBucket = await TransparentUpgradeableProxyImplementationBucket.getAddress();
+    console.log("Deployed transparentUpgradeableProxyImplementationBucket", transparentUpgradeableProxyImplementationBucket);
 
     console.log("Deploying PoolFactory Upgradable...");
     const PoolFactory = await ethers.getContractFactory("PoolFactory");
-    const poolFactory = await upgrades.deployProxy(PoolFactory, [refereeAddress, esXaiAddress, deployerAddress, poolImplAddress, bucketImplAddress], { kind: "transparent", deployer });
+    const poolFactory = await upgrades.deployProxy(PoolFactory, [config.refereeAddress, config.esXaiAddress, deployerAddress, poolImplAddress, bucketImplAddress], { kind: "transparent", deployer });
     const tx = await poolFactory.deploymentTransaction();
     await tx.wait(3);
     const poolFactoryAddress = await poolFactory.getAddress();
     console.log("PoolFactory deployed to:", poolFactoryAddress);
 
-    //Upgrade the referee
-    const referee = await ethers.getContractFactory("Referee5");
-    console.log("Got factory");
-    await upgrades.upgradeProxy(refereeAddress, referee, { call: { fn: "initialize", args: [poolFactoryAddress] } });
-    console.log("Upgraded");
-
-    //Give PoolFactory auth to whitelist new pools & buckets on esXai
-    const esXai = await new ethers.Contract(esXaiAddress, esXaiAbi, deployer);
+    // Give PoolFactory auth to whitelist new pools & buckets on esXai
+    console.log("Adding esXai DEFAULT_ADMIN_ROLE to PoolFactory...");
+    const esXai = await new ethers.Contract(config.esXaiAddress, esXaiAbi, deployer);
     const esXaiAdminRole = await esXai.DEFAULT_ADMIN_ROLE();
     await esXai.grantRole(esXaiAdminRole, poolFactoryAddress);
+    console.log("Granted role");
 
-    await safeVerify({ contract: poolFactory });
+    // Upgrade the referee
+    const referee = await ethers.getContractFactory("Referee5");
+    console.log("Got factory");
+    await upgrades.upgradeProxy(config.refereeAddress, referee, { call: { fn: "initialize", args: [poolFactoryAddress] } });
+    console.log("Upgraded");
 
     await run("verify:verify", {
-        address: refereeAddress,
+        address: poolFactoryAddress,
+        constructorArguments: [],
+        contract: "PoolFactory"
+    });
+
+    await run("verify:verify", {
+        address: config.refereeAddress,
         constructorArguments: [],
         contract: "Referee5"
     });
@@ -58,6 +77,18 @@ async function main() {
     await run("verify:verify", {
         address: poolImplAddress,
         constructorArguments: [],
+    });
+
+    await run("verify:verify", {
+        address: transparentUpgradeableProxyImplementationPool,
+        constructorArguments: [poolImplAddress, deployerAddress, "0x"],
+        contract: "contracts/staking-v2/TransparentUpgradable.sol:TransparentUpgradeableProxyImplementation"
+    });
+
+    await run("verify:verify", {
+        address: transparentUpgradeableProxyImplementationBucket,
+        constructorArguments: [bucketImplAddress, deployerAddress, "0x"],
+        contract: "contracts/staking-v2/TransparentUpgradable.sol:TransparentUpgradeableProxyImplementation"
     });
 
     console.log("verified")
