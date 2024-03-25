a122b0d7ff6bb4b1e929b25b0a4475f704e11ccf
CryptIT Austria <office@cryptit.at>
2024-03-25 02:00:47 +0100

    restructure deployment script

--- infrastructure/smart-contracts/scripts/deployStakingV2.mjs
+++ infrastructure/smart-contracts/scripts/deployStakingV2.mjs
@@ -14,35 +14,111 @@ async function main() {
     await StakingPool.waitForDeployment();
     const poolImplAddress = await StakingPool.getAddress();
     console.log("Deployed Impl", poolImplAddress)
+    
+    await run("verify:verify", {
+        address: poolImplAddress,
+        constructorArguments: [],
+    });
 
-    // // //DEPLOY BUCKET TRACKER IMPL
+    // // //DEPLOY BUCKET TRACKER KEY IMPL
     console.log("Deploying BucketTracker implementation...");
-    const BucketTracker = await ethers.deployContract("BucketTracker");
-    await BucketTracker.waitForDeployment();
-    const bucketImplAddress = await BucketTracker.getAddress();
-    console.log("Deployed Impl", bucketImplAddress)
+    const KeyBucketTracker = await ethers.deployContract("BucketTracker");
+    await KeyBucketTracker.waitForDeployment();
+    const keyBucketImplAddress = await KeyBucketTracker.getAddress();
+    console.log("Deployed Impl", keyBucketImplAddress)
+    
+    await run("verify:verify", {
+        address: keyBucketImplAddress,
+        constructorArguments: [],
+    });
+
+    // // //DEPLOY BUCKET TRACKER ESXAI IMPL
+    console.log("Deploying BucketTracker implementation...");
+    const EsXaiBucketTracker = await ethers.deployContract("BucketTracker");
+    await EsXaiBucketTracker.waitForDeployment();
+    const esXaiBucketImplAddress = await EsXaiBucketTracker.getAddress();
+    console.log("Deployed Impl", esXaiBucketImplAddress)
+
+    await run("verify:verify", {
+        address: esXaiBucketImplAddress,
+        constructorArguments: [],
+    });
+
+    //TODO DEPLOY BEACON
+    console.log("Deploying PoolBeacon");
+    const PoolBeacon = await ethers.deployContract("PoolBeacon", [poolImplAddress]);
+    await PoolBeacon.waitForDeployment();
+    const poolBeaconAddress = await PoolBeacon.getAddress();
+    console.log("Deployed PoolBeacon", poolBeaconAddress);
+
+    await run("verify:verify", {
+        address: poolBeaconAddress,
+        constructorArguments: [poolImplAddress],
+    });
+
+    console.log("Deploying BeaconProxy for PoolBeacon for auto verification");
+    const PoolBeaconProxy = await ethers.deployContract("BeaconProxy", [poolBeaconAddress, "0x"]);
+    await PoolBeaconProxy.waitForDeployment();
+    const poolBeaconProxyAddress = await PoolBeaconProxy.getAddress();
+    console.log("Deployed BeaconProxy for PoolBeacon", poolBeaconProxyAddress);
+
+    await run("verify:verify", {
+        address: poolBeaconProxyAddress,
+        constructorArguments: [poolBeaconAddress, "0x"],
+    });
+
+    console.log("Deploying EsXaiBucketBeacon");
+    const EsXaiBucketBeacon = await ethers.deployContract("PoolBeacon", [esXaiBucketImplAddress]);
+    await EsXaiBucketBeacon.waitForDeployment();
+    const esXaiBucketBeaconAddress = await EsXaiBucketBeacon.getAddress();
+    console.log("Deployed EsXaiBucketBeacon", esXaiBucketBeaconAddress);
+
+    await run("verify:verify", {
+        address: esXaiBucketBeaconAddress,
+        constructorArguments: [esXaiBucketImplAddress],
+    });
+
+    console.log("Deploying BeaconProxy for EsXaiBucketBeaconProxy for auto verification");
+    const EsXaiBucketBeaconProxy = await ethers.deployContract("BeaconProxy", [esXaiBucketBeaconAddress, "0x"]);
+    await EsXaiBucketBeaconProxy.waitForDeployment();
+    const esXaiBucketBeaconProxyAddress = await EsXaiBucketBeaconProxy.getAddress();
+    console.log("Deployed BeaconProxy for EsXaiBucketBeaconProxy", esXaiBucketBeaconProxyAddress);
+
+    await run("verify:verify", {
+        address: esXaiBucketBeaconProxyAddress,
+        constructorArguments: [esXaiBucketBeaconAddress, "0x"],
+    });
+
+    console.log("Deploying KeyBucketBeacon");
+    const KeyBucketBeacon = await ethers.deployContract("PoolBeacon", [keyBucketImplAddress]);
+    await KeyBucketBeacon.waitForDeployment();
+    const keyBucketBeaconAddress = await KeyBucketBeacon.getAddress();
+    console.log("Deployed KeyBucketBeacon", keyBucketBeaconAddress);
+
+    await run("verify:verify", {
+        address: keyBucketBeaconAddress,
+        constructorArguments: [keyBucketImplAddress],
+    });
 
 
-    //ONLY NEEDED IN PROD DEPLOY FOR AUTO VERIFICATION FROM FACTORY CONTRACTS
-    console.log("Deploying TransparentUpgradeableProxyImplementationPool...");
-    const TransparentUpgradeableProxyImplementationPool = await ethers.deployContract("TransparentUpgradeableProxyImplementation", [poolImplAddress, deployerAddress, "0x"]);
-    await TransparentUpgradeableProxyImplementationPool.waitForDeployment();
-    const transparentUpgradeableProxyImplementationPool = await TransparentUpgradeableProxyImplementationPool.getAddress();
-    console.log("Deployed TransparentUpgradeableProxyImplementationPool", transparentUpgradeableProxyImplementationPool);
+    console.log("Deploying BeaconProxy for KeyBucketBeaconProxy for auto verification");
+    const KeyBucketBeaconProxy = await ethers.deployContract("BeaconProxy", [keyBucketBeaconAddress, "0x"]);
+    await KeyBucketBeaconProxy.waitForDeployment();
+    const keyBucketBeaconProxyAddress = await KeyBucketBeaconProxy.getAddress();
+    console.log("Deployed BeaconProxy for KeyBucketBeaconProxy", keyBucketBeaconProxyAddress);
 
-    //ONLY NEEDED IN PROD DEPLOY FOR AUTO VERIFICATION FROM FACTORY CONTRACTS
-    console.log("Deploying transparentUpgradeableProxyImplementationBucket...");
-    const TransparentUpgradeableProxyImplementationBucket = await ethers.deployContract("TransparentUpgradeableProxyImplementation", [bucketImplAddress, deployerAddress, "0x"]);
-    await TransparentUpgradeableProxyImplementationBucket.waitForDeployment();
-    const transparentUpgradeableProxyImplementationBucket = await TransparentUpgradeableProxyImplementationBucket.getAddress();
-    console.log("Deployed transparentUpgradeableProxyImplementationBucket", transparentUpgradeableProxyImplementationBucket);
+    await run("verify:verify", {
+        address: keyBucketBeaconProxyAddress,
+        constructorArguments: [keyBucketBeaconAddress, "0x"],
+    });
+
 
     console.log("Deploying PoolFactory Upgradable...");
     const PoolFactory = await ethers.getContractFactory("PoolFactory");
 
     const poolFactory = await upgrades.deployProxy(
         PoolFactory,
-        [config.refereeAddress, config.esXaiAddress, config.nodeLicenseAddress, deployerAddress, poolImplAddress, bucketImplAddress],
+        [config.refereeAddress, config.esXaiAddress, config.nodeLicenseAddress],
         { kind: "transparent", deployer }
     );
 
@@ -50,11 +126,49 @@ async function main() {
     await tx.wait(3);
     const poolFactoryAddress = await poolFactory.getAddress();
     console.log("PoolFactory deployed to:", poolFactoryAddress);
+    
+    await run("verify:verify", {
+        address: poolFactoryAddress,
+        constructorArguments: [],
+        contract: "PoolFactory"
+    });
 
-    //TODO DEPLOY PoolProxyDeployer upgradable with init
-    //TODO DEPLOY PoolProxyDeployer upgradable with init
-    //TODO DEPLOY PoolProxyDeployer upgradable with init
-    //TODO DEPLOY PoolProxyDeployer upgradable with init
+    
+    console.log("Deploying PoolProxyDeployer Upgradable...");
+    const PoolProxyDeployer = await ethers.getContractFactory("PoolProxyDeployer");
+
+    const poolProxyDeployer = await upgrades.deployProxy(
+        PoolProxyDeployer,
+        [poolFactoryAddress, poolBeaconAddress, keyBucketBeaconAddress, esXaiBucketBeaconAddress],
+        { kind: "transparent", deployer }
+    );
+
+    const tx2 = await poolProxyDeployer.deploymentTransaction();
+    await tx2.wait(3);
+    const poolProxyDeployerAddress = await poolProxyDeployer.getAddress();
+    console.log("PoolProxyDeployer deployed to:", poolProxyDeployerAddress);
+    
+    await run("verify:verify", {
+        address: poolProxyDeployer,
+        constructorArguments: [],
+        contract: "PoolProxyDeployer"
+    });
+
+    console.log("Update PoolFactory updatePoolProxyDeployer...");
+    await poolFactory.updatePoolProxyDeployer(poolProxyDeployerAddress);
+    console.log("Updated PoolFactory added poolProxyDeployerAddress", poolProxyDeployerAddress);
+
+    // Upgrade the referee
+    const referee = await ethers.getContractFactory("Referee5");
+    console.log("Got factory");
+    await upgrades.upgradeProxy(config.refereeAddress, referee, { call: { fn: "initialize", args: [poolFactoryAddress] } });
+    console.log("Upgraded");
+
+    await run("verify:verify", {
+        address: config.refereeAddress,
+        constructorArguments: [],
+        contract: "Referee5"
+    });
 
     // Give PoolFactory auth to whitelist new pools & buckets on esXai
     console.log("Adding esXai DEFAULT_ADMIN_ROLE to PoolFactory...");
@@ -67,47 +181,7 @@ async function main() {
     console.log("Added PoolFactory to esXai whitelist");
 
 
-    // Upgrade the referee
-    const referee = await ethers.getContractFactory("Referee5");
-    console.log("Got factory");
-    await upgrades.upgradeProxy(config.refereeAddress, referee, { call: { fn: "initialize", args: [poolFactoryAddress] } });
-    console.log("Upgraded");
-
-    await run("verify:verify", {
-        address: poolFactoryAddress,
-        constructorArguments: [],
-        contract: "PoolFactory"
-    });
-
-    await run("verify:verify", {
-        address: config.refereeAddress,
-        constructorArguments: [],
-        contract: "Referee5"
-    });
-
-    await run("verify:verify", {
-        address: bucketImplAddress,
-        constructorArguments: [],
-    });
-
-    await run("verify:verify", {
-        address: poolImplAddress,
-        constructorArguments: [],
-    });
-
-    await run("verify:verify", {
-        address: transparentUpgradeableProxyImplementationPool,
-        constructorArguments: [poolImplAddress, deployerAddress, "0x"],
-        contract: "contracts/staking-v2/TransparentUpgradable.sol:TransparentUpgradeableProxyImplementation"
-    });
-
-    await run("verify:verify", {
-        address: transparentUpgradeableProxyImplementationBucket,
-        constructorArguments: [bucketImplAddress, deployerAddress, "0x"],
-        contract: "contracts/staking-v2/TransparentUpgradable.sol:TransparentUpgradeableProxyImplementation"
-    });
-
-    console.log("verified")
+    console.log("Deployed Staking V2 setup")
 }
 
 
