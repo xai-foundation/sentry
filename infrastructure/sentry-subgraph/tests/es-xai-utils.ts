074fa081d5b9b961e78ab7ca2f66968bb17d172f
Christopher Powroznik <christopher@frameonesoftware.com>
2024-03-26 13:57:18 -0700

    before a refactor

---
+++ infrastructure/sentry-subgraph/tests/es-xai-utils.ts
@@ -0,0 +1,55 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  BeaconUpgraded,
+  Upgraded
+} from "../generated/esXai/esXai"
+
+export function createAdminChangedEvent(
+  previousAdmin: Address,
+  newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdmin",
+      ethereum.Value.fromAddress(previousAdmin)
+    )
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
+
+  beaconUpgradedEvent.parameters = new Array()
+
+  beaconUpgradedEvent.parameters.push(
+    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+  )
+
+  return beaconUpgradedEvent
+}
+
+export function createUpgradedEvent(implementation: Address): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementation",
+      ethereum.Value.fromAddress(implementation)
+    )
+  )
+
+  return upgradedEvent
+}
