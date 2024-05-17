8d800ea99a6d41027b874618b16e1a53621e4d12
IhorCryptit <ik@cryptit.at>
2024-05-17 18:05:44 +0300

    fix: fixed cookie banner styles

--- apps/web-connect/src/features/footer/ReactCookieConsent.tsx
+++ apps/web-connect/src/features/footer/ReactCookieConsent.tsx
@@ -10,13 +10,13 @@ export function ReactCookieConsent({ }) {
 			buttonText="I AGREE"
 			contentClasses="!m-0 px-4 sm:flex-col lg:flex-row justify-center items-center"
 			containerClasses="cookie-consent-container"
-			style={{ backgroundColor:" rgba(24, 20, 21, 0.70)", textAlign: "center", fontFamily: "Rajdhani, sans-serif", margin: "0", fontWeight: "600", padding: "0", justifyContent: "center", alignItems: "center", clipPath: "polygon(15px 0, 100% 0, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0 100%, 0 15px)", minHeight: "51px", maxWidth: "100%" }}
+			style={{ backgroundColor:" rgba(24, 20, 21, 0.70)", textAlign: "center", fontFamily: "Rajdhani, sans-serif", margin: "0", fontWeight: "600", padding: "0", justifyContent: "center", alignItems: "center", clipPath: "polygon(15px 0, 100% 0, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0 100%, 0 15px)", minHeight: "50px", maxWidth: "100%" }}
 			buttonStyle={{ color: "black", fontSize: "20px", background: "#F30919", fontFamily: "Rajdhani, sans-serif", padding: "10px 40px", clipPath: "polygon(15px 0, 100% 0, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0 100%, 0 15px)", fontWeight: "700", margin: "0" }}
 			expires={150} // Cookie expiry in days
 		>
 			<div className="cookie-consent flex items-center text-white text-[18px] lg:w-[90%] opacity-70 !m-0">
 				<div className="min-w-[30px] mr-2"><WarningIcon fill="#FF0030" width={30} height={25}/></div>
-				<div className="sm:mb-4 md:mb-0 lg:mb-0">This website uses cookies to enhance the user experience. By staying on this website you agree to our <a target="_blank" className="mx-2 hover:text-white duration-200 ease-in" href="https://xai.games/generalterms">Terms of Agreement</a> and <a target="_blank" className="ml-2 hover:text-white duration-200 ease-in" href="https://xai.games/privacypolicy">Privacy Policy</a></div>
+				<div className="sm:mb-4 md:mb-0 lg:mb-0">This website uses cookies to enhance the user experience. By staying on this website you agree to our <a target="_blank" className="mx-2 hover:text-white duration-200 ease-in font-bold" href="https://xai.games/generalterms">Terms of Agreement</a> and <a target="_blank" className="ml-2 hover:text-white duration-200 ease-in font-bold" href="https://xai.games/privacypolicy">Privacy Policy</a></div>
 			</div>
 		</CookieConsent>
 	);
