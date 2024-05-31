a725407e50e8f62ac2d0145d4ce904c3b01a8fed
IhorCryptit <ik@cryptit.at>
2024-05-31 18:59:51 +0300

    fix: added custom tooltip

--- packages/ui/src/rebrand/index.ts
+++ packages/ui/src/rebrand/index.ts
@@ -1,3 +1,4 @@
-export * from './buttons'
-export * from './links'
-export * from './callout'
+export * from "./buttons";
+export * from "./links";
+export * from "./callout";
+export * from "./tooltip";
