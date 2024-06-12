2708826044b5cb63f40ba0cdeb336c973869b5d1
IhorCryptit <ik@cryptit.at>
2024-06-12 16:36:45 +0300

    fix: fixed margins, padding, width, height of components

--- apps/sentry-client-desktop/src/features/keys/modals/buy-keys/BuyKeysDrawer.tsx
+++ apps/sentry-client-desktop/src/features/keys/modals/buy-keys/BuyKeysDrawer.tsx
@@ -9,7 +9,7 @@ export function BuyKeysDrawer() {
 	return (
 		<div className="w-full h-full flex flex-col justify-start items-center">
 			<div
-				className="w-full h-16 min-h-[64px] flex flex-row justify-between items-center border-b border-primaryBorderColor text-2xl text-white font-bold px-6 py-7">
+				className="w-full flex flex-row justify-between items-center border-b border-primaryBorderColor text-2xl text-white font-bold px-6 py-6">
 				<span>Purchase key</span>
 				<div className="cursor-pointer z-10" onClick={() => setDrawerState(null)}>
 					<AiOutlineClose size={20}/>
