f1e38fcd4a296c659c9ef1761373ead66cc167f8
IhorCryptit <ik@cryptit.at>
2024-06-18 15:43:29 +0300

    fix: removed borders

--- apps/sentry-client-desktop/src/features/home/modals/ExportSentryDrawer.tsx
+++ apps/sentry-client-desktop/src/features/home/modals/ExportSentryDrawer.tsx
@@ -46,7 +46,7 @@ export function ExportSentryDrawer() {
 		<div>
 			<div className="h-full flex flex-col justify-start items-center">
 				<div
-					className="w-full flex flex-row text-white justify-between items-center border-b border-border-chromaphobicBlack">
+					className="w-full flex flex-row text-white justify-between items-center border-b border-chromaphobicBlack">
 					<div className="flex flex-row gap-2 justify-between w-full items-center text-2xl font-bold px-8 py-[24px]">
 						<span>Export Sentry Wallet</span>
 						<span
