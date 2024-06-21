66ce1c6663235de4e466f5f4ff2cb5f5847aaae6
Jerry Hodges <jerryhodges@expopulus.com>
2024-06-21 11:08:58 -0400

    Remove Unused Imports

--- apps/web-connect/src/features/checkout/hooks/index.ts
+++ apps/web-connect/src/features/checkout/hooks/index.ts
@@ -1,5 +1,3 @@
-export * from './useGetErc20Allowance';
-export * from './useGetErc20Balance';
 export * from './useGetExchangeRate';
 export * from './useGetPriceForQuantity';
 export * from './useGetTotalSupplyAndCap';
