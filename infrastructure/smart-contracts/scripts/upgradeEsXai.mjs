7a177340b5f9a5fac5cd93f4aa3bebadcb13daa6
Jerry Hodges <jerryhodges@expopulus.com>
2024-06-22 19:00:30 -0400

    Added KYC Check Removal for EsXai Claim

--- infrastructure/smart-contracts/scripts/upgradeEsXai.mjs
+++ infrastructure/smart-contracts/scripts/upgradeEsXai.mjs
@@ -4,15 +4,15 @@ const { ethers, upgrades } = hardhat;
 const address = "0x4C749d097832DE2FEcc989ce18fDc5f1BD76700c";
 const foundationReceiver = "0x1F941F7Fb552215af81e6bE87F59578C18783483"; // TODO double check this address
 const foundationBasePoints = BigInt(500);
-
+const maxKeys = BigInt(10);
 
 async function main() {
     const [deployer] = (await ethers.getSigners());
     const deployerAddress = await deployer.getAddress();
     console.log("Deployer address", deployerAddress);
-    const esXai2 = await ethers.getContractFactory("esXai2");
+    const esXai3 = await ethers.getContractFactory("esXai3");
     console.log("Got factory");
-    await upgrades.upgradeProxy(address, esXai2, { call: { fn: "initialize", args: [foundationReceiver, foundationBasePoints] } });
+    await upgrades.upgradeProxy(address, esXai3, { call: { fn: "initialize", args: [foundationReceiver, foundationBasePoints, config.refereeAddress, config.nodeLicenseAddress, maxKeys] } });
     console.log("Upgraded");
 
     await run("verify:verify", {
