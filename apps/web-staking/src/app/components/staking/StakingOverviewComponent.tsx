5098a7998e2d30e66357c053776471a5abb14dfa
Stanyslav P <sp@cryptit.at>
2024-07-11 16:44:23 +0300

    added success redirect after minting

--- apps/web-staking/src/app/components/staking/StakingOverviewComponent.tsx
+++ apps/web-staking/src/app/components/staking/StakingOverviewComponent.tsx
@@ -8,16 +8,16 @@ import StakedPoolsTable from "./StakedPoolsTable";
 import ClaimableRewardsComponent from "./ClaimableRewardsComponent";
 import MainTitle from "../titles/MainTitle";
 import { PagedPools } from "@/server/services/Pool.service";
-import { useSearchParams } from "next/navigation";
+import {useSearchParams} from "next/navigation";
 import { useRouter } from "next/navigation";
 import AgreeModalComponent from "../modal/AgreeModalComponents";
-import { useGetMaxKeyPerPool, useGetTotalStakedHooks, useGetUserInteractedPools } from "@/app/hooks";
+import { useGetMaxKeyPerPool, useGetTotalStakedHooks, useGetUserInteractedPools } from "@/app/hooks/hooks";
 import { WriteFunctions, executeContractWrite } from "@/services/web3.writes";
 import { loadingNotification, updateNotification } from "../notifications/NotificationsComponent";
 import { getNetwork, getTotalClaimAmount, mapWeb3Error } from "@/services/web3.service";
 import { Id } from "react-toastify";
 import { useGetTiers } from "@/app/hooks/useGetTiers";
-import MessageModalComponent from "../modal/MessageModalComponent";
+import {BaseModal} from "@/app/components/ui";
 
 export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPools }) => {
   const router = useRouter();
@@ -38,7 +38,6 @@ export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPool
   const { writeContractAsync } = useWriteContract();
   const [receipt, setReceipt] = useState<`0x${string}` | undefined>();
   const toastId = useRef<Id>();
-  const [mintSuccessfulRedirect, setMintSuccessfulRedirect] = useState(searchParams.get("mint") ? searchParams.get("mint") === "true" : false);
 
   // Substitute Timeouts with useWaitForTransaction
   const { data, isError, isLoading: transactionLoading, isSuccess, status } = useWaitForTransactionReceipt({
@@ -51,17 +50,17 @@ export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPool
 
   const updateOnSuccess = useCallback(() => {
     updateNotification(
-      "Successfully claimed",
-      toastId.current as Id,
-      false,
-      receipt,
-      chainId
+        "Successfully claimed",
+        toastId.current as Id,
+        false,
+        receipt,
+        chainId
     );
 
     getTotalClaimAmount(getNetwork(chainId), userPools.map(p => p.address), address!)
-      .then(totalClaim => {
-        setCurrentTotalClaimableAmount(totalClaim);
-      });
+        .then(totalClaim => {
+          setCurrentTotalClaimableAmount(totalClaim);
+        });
   }, [receipt, chainId, userPools, address])
 
   const updateOnError = useCallback(() => {
@@ -89,14 +88,14 @@ export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPool
     try {
 
       setReceipt(await executeContractWrite(
-        WriteFunctions.claimFromPools,
-        [poolsToClaim.slice(0, 10)], //TODO test if this works for more than 10 pools in 1 transaction
-        chainId,
-        writeContractAsync,
-        switchChain
+          WriteFunctions.claimFromPools,
+          [poolsToClaim.slice(0, 10)], //TODO test if this works for more than 10 pools in 1 transaction
+          chainId,
+          writeContractAsync,
+          switchChain
       ) as `0x${string}`);
 
-    } catch (ex: unknown) {
+    } catch (ex: any) {
       const error = mapWeb3Error(ex);
       updateNotification(error, toastId.current as Id, true);
     }
@@ -109,21 +108,21 @@ export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPool
   const submitSearch = () => {
     setCurrentPage(1);
     router.push(buildURI(searchValue, 1, showTableKeys, hideFullKeys, hideFullEsXai),
-      { scroll: false }
+        { scroll: false }
     );
   };
 
   const setPage = (page: number) => {
     setCurrentPage(page);
     router.push(buildURI(searchValue, page, showTableKeys, hideFullKeys, hideFullEsXai),
-      { scroll: false }
+        { scroll: false }
     );
   };
 
   const onToggleShowKeys = (showKeys: boolean) => {
     setShowTableKeys(showKeys);
     router.push(buildURI(searchValue, currentPage, showKeys, hideFullKeys, hideFullEsXai),
-      { scroll: false }
+        { scroll: false }
     );
   };
 
@@ -131,57 +130,74 @@ export const StakingOverviewComponent = ({ pagedPools }: { pagedPools: PagedPool
     if (showTableKeys) {
       setHideFullKeys(checked);
       router.push(buildURI(searchValue, currentPage, showTableKeys, checked, hideFullEsXai),
-        { scroll: false }
+          { scroll: false }
       );
     } else {
       setHideFullEsXai(checked);
       router.push(buildURI(searchValue, currentPage, showTableKeys, hideFullKeys, checked),
-        { scroll: false }
+          { scroll: false }
       );
     }
   }
 
+  const isModal = useSearchParams().get("modal");
+  const clearedPathname = window.location.href.split("&")[0];
+
+  const onModalSubmit = () => {
+    router.replace(clearedPathname);
+  };
+
+
   return (
-    <div className="relative flex sm:flex-col items-start lg:px-6 sm:px-0 sm:w-full">
-      <AgreeModalComponent address={address} />
-      <MessageModalComponent isOpen={mintSuccessfulRedirect} setOpen={() => setMintSuccessfulRedirect(false)} modalText="Stake your key in a pool to start earning rewards!" />
-      <div className="flex justify-between w-full flex-col xl:flex-row sm:mb-[70px] lg:mb-6 xl:mb-3">
-        <MainTitle title={"Staking"} classNames="sm:indent-4 lg:indent-0" />
+      <div className="relative flex sm:flex-col items-start lg:px-6 sm:px-0 sm:w-full">
+        <AgreeModalComponent address={address} />
+        <div className="flex justify-between w-full flex-col xl:flex-row sm:mb-[70px] lg:mb-6 xl:mb-3">
+          <MainTitle title={"Staking"} classNames="sm:indent-4 lg:indent-0" />
 
-        {address && <div className="sm:w-[91%] absolute sm:right-[17px] sm:top-[85px] lg:right-[55px] lg:top-6 lg:w-[450px] shadow-light"><ClaimableRewardsComponent
-          disabled={isLoading || transactionLoading || currentTotalClaimableAmount === 0}
-          totalClaimAmount={currentTotalClaimableAmount}
-          onClaim={onClaimAll}
-        /></div>}
+          {address && <div className="sm:w-[91%] absolute sm:right-[17px] sm:top-[85px] lg:right-[55px] lg:top-6 lg:w-[450px] shadow-light"><ClaimableRewardsComponent
+              disabled={isLoading || transactionLoading || currentTotalClaimableAmount === 0}
+              totalClaimAmount={currentTotalClaimableAmount}
+              onClaim={onClaimAll}
+          /></div>}
 
+        </div>
+
+        <BaseModal
+            isOpened={!!isModal}
+            modalHeader={"Stake keys in a pool to earn rewards"}
+            modalBody={"Choose a pool and stake keys in the pool to start earning rewards"}
+            closeModal={onModalSubmit}
+            submitText={"Start staking"}
+            withOutCancelButton
+            onSubmit={onModalSubmit}
+        />
+
+        {(address && (userPools.length > 0 || totalStaked > 0)) && <StakedPoolsTable v1Stake={totalStaked} v1MaxStake={maxStakedCapacity} userPools={userPools} tiers={tiers} showTableKeys={showTableKeys} maxKeyPerPool={maxKeyPerPool} />}
+
+        <SearchBarComponent
+            searchValue={searchValue}
+            showTableKeys={showTableKeys}
+            setSearchValue={setSearchValue}
+            setShowKeyInfo={setShowTableKeys}
+            setClickSearch={submitSearch}
+            filterCheckbox={showTableKeys ? hideFullKeys : hideFullEsXai}
+            setFilterCheckbox={onClickFilterCB}
+            onToggleShowKeys={onToggleShowKeys}
+            showedPools={pagedPools.count}
+            hiddenPools={pagedPools.totalPoolsInDB - pagedPools.count}
+            userPools={userPools.length}
+        />
+
+        <AvailablePoolsTableComponent
+            showTableKeys={showTableKeys}
+            pools={pagedPools.poolInfos}
+            page={currentPage}
+            totalPages={Math.ceil(pagedPools.count / 10)}
+            setPage={setPage}
+            address={address}
+            tiers={tiers}
+            maxKeyPerPool={maxKeyPerPool}
+        />
       </div>
-
-      {(address && (userPools.length > 0 || totalStaked > 0)) && <StakedPoolsTable v1Stake={totalStaked} v1MaxStake={maxStakedCapacity} userPools={userPools} tiers={tiers} showTableKeys={showTableKeys} maxKeyPerPool={maxKeyPerPool} />}
-
-      <SearchBarComponent
-        searchValue={searchValue}
-        showTableKeys={showTableKeys}
-        setSearchValue={setSearchValue}
-        setShowKeyInfo={setShowTableKeys}
-        setClickSearch={submitSearch}
-        filterCheckbox={showTableKeys ? hideFullKeys : hideFullEsXai}
-        setFilterCheckbox={onClickFilterCB}
-        onToggleShowKeys={onToggleShowKeys}
-        showedPools={pagedPools.count}
-        hiddenPools={pagedPools.totalPoolsInDB - pagedPools.count}
-        userPools={userPools.length}
-      />
-
-      <AvailablePoolsTableComponent
-        showTableKeys={showTableKeys}
-        pools={pagedPools.poolInfos}
-        page={currentPage}
-        totalPages={Math.ceil(pagedPools.count / 10)}
-        setPage={setPage}
-        address={address}
-        tiers={tiers}
-        maxKeyPerPool={maxKeyPerPool}
-      /> 
-    </div>
   );
 }
