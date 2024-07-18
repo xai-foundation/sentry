36e338924bf7bac21117e7aeb8ff207397849f09
Jerry Hodges <jerryhodges@expopulus.com>
2024-07-18 10:38:37 -0400

    Updated RPC Queries for Pool Submissions

--- packages/core/src/operator/operatorRuntime.ts
+++ packages/core/src/operator/operatorRuntime.ts
@@ -13,19 +13,15 @@ import {
     getSentryKeysFromGraph,
     getPoolInfosFromGraph,
     getOwnerOrDelegatePools,
-    getKeysOfPool,
-    getUserInteractedPools,
-    getUserStakedKeysOfPool,
-    listNodeLicenses,
     getLatestChallenge,
     getBoostFactor as getBoostFactorRPC,
     getMintTimestamp,
     listOwnersForOperator,
-    checkKycStatus,
     getSubmissionsForChallenges,
     getUnStakedKeysOfUser,
     submitPoolAssertions,
-    claimPoolSubmissionRewards
+    claimPoolSubmissionRewards,
+    getMultipleUsersInteractedPoolsRpc
 
 } from "../index.js";
 import axios from "axios";
@@ -294,7 +290,7 @@ async function processNewChallenge(
     // Process any Pool Submissions
     if(cachedPoolsOfOperator && cachedPoolsOfOperator.length > 0) {
             try {
-                    await submitPoolAssertions(cachedPoolsOfOperator, challengeNumber, challenge.assertionStateRootOrConfirmData, cachedSigner, cachedLogger);
+                await submitPoolAssertions(cachedPoolsOfOperator, challengeNumber, challenge.assertionStateRootOrConfirmData, cachedSigner, cachedLogger);
             } catch (error: any) {
                 cachedLogger(`Error submitting pool assertions for challenge ${challengeNumber} - ${error && error.message ? error.message : error}`);
             }    
@@ -489,48 +485,17 @@ async function listenForChallengesCallback(challengeNumber: bigint, challenge: C
     } catch (error: any) {
         cachedLogger(`Error processing new challenge in listener callback: - ${error && error.message ? error.message : error}`);
     }
-
-
 }
 
 
-//Set the assignedPool field to our owner keys should they be staked
-async function syncOwnerStakedKeysForRPC(owners: string[], sentryKeysMap: { [keyId: string]: SentryKey }): Promise<{ [keyId: string]: SentryKey }> {
 
-    for (const owner of owners) {
-        const ownerInteractedPools = await getUserInteractedPools(owner);
-
-        if (ownerInteractedPools.length) {
-
-            for (const pool of ownerInteractedPools) {
-
-                const keys = await getUserStakedKeysOfPool(pool, owner);
-                for (const key of keys) {
-
-                    //The key needs to be in the list already
-                    //We just need to check if we already mapped the pool because it was staked in one of the operators owned / delegated pools
-
-                    if (!sentryKeysMap[key.toString()]) {
-                        cachedLogger(`Error key not found in list of keys to operate, restart the operator to reload for \nowner: ${owner}, \npool: ${pool}, \nkey: ${key.toString()}.`);
-                        continue;
-                    }
-
-                    sentryKeysMap[key.toString()].assignedPool = pool;
-                }
-            }
-        }
-    }
-
-    return sentryKeysMap;
-}
 
 //Load all the keys from pool our operator should operate
 //Exclude pool that are not in the whitelist
 const reloadPoolKeysForRPC = async (
     operator: string,
-    sentryKeysMap: { [keyId: string]: SentryKey },
     operatorOwners?: string[]
-): Promise<{ [keyId: string]: SentryKey }> => {
+): Promise<string[]> => {
 
     let operatorPoolAddresses = await getOwnerOrDelegatePools(operator);
 
@@ -538,45 +503,7 @@ const reloadPoolKeysForRPC = async (
         operatorPoolAddresses = operatorPoolAddresses.filter(o => operatorOwners.includes(o.toLowerCase()))
     }
 
-    if (operatorPoolAddresses.length) {
-
-        cachedLogger(`Found ${operatorPoolAddresses.length} pools for operator.`);
-
-        for (const pool of operatorPoolAddresses) {
-            //Check every key and find out if its already in the nodeLicenseIds list
-            cachedLogger(`Fetching node licenses for pool ${pool}.`);
-
-            const keys = await getKeysOfPool(pool);
-
-            for (const key of keys) {
-
-
-                if (sentryKeysMap[key.toString()]) {
-
-                    sentryKeysMap[key.toString()].assignedPool = pool;
-
-                } else {
-
-                    if (!mintTimestamps[key.toString()]) {
-                        mintTimestamps[key.toString()] = await retry(async () => await getMintTimestamp(key))
-                    }
-
-                    sentryKeysMap[key.toString()] = {
-                        assignedPool: pool,
-                        keyId: key,
-                        mintTimeStamp: mintTimestamps[key.toString()],
-                        id: key.toString(),
-                        owner: "",
-                        sentryWallet: {} as any,
-                        submissions: []
-                    }
-                }
-
-            }
-        }
-    }
-
-    return sentryKeysMap;
+    return operatorPoolAddresses;
 }
 
 
@@ -755,15 +682,17 @@ const loadOperatorKeysFromRPC = async (
         cachedKeysOfOwner = {};
 
         for (const owner of owners) {
-            cachedLogger(`Fetching node licenses for owner ${owner}.`);
-            const licensesOfOwner = await listNodeLicenses(owner, (keyId) => {
-                cachedLogger(`Fetched node licenses key ${keyId.toString()}.`);
-            });
+            cachedLogger(`Fetching node licenses for owner ${owner}.`);       
+            const unstakedLicensesOfOwner = await getUnStakedKeysOfUser(owner);
 
-            for (const licenseId of licensesOfOwner) {
+            for (const licenseId of unstakedLicensesOfOwner) {
 
-                if (!mintTimestamps[licenseId.toString()]) {
-                    mintTimestamps[licenseId.toString()] = await retry(async () => await getMintTimestamp(licenseId))
+                if (!mintTimestamps[licenseId.toString()]) {   
+                    if(licenseId < 35000){                          
+                        mintTimestamps[licenseId.toString()] = 0n;
+                    }else{      
+                        mintTimestamps[licenseId.toString()] = await retry(async () => await getMintTimestamp(licenseId))
+                    }
                 }
 
                 const sentryKey: SentryKey = {
@@ -779,8 +708,8 @@ const loadOperatorKeysFromRPC = async (
                 sentryKeysMap[licenseId.toString()] = sentryKey;
             }
 
-            if (licensesOfOwner.length) {
-                cachedLogger(`Fetched ${licensesOfOwner.length} node licenses for owner ${owner}.`);
+            if (unstakedLicensesOfOwner.length) {
+                cachedLogger(`Fetched ${unstakedLicensesOfOwner.length} node licenses for owner ${owner}.`);
             }
         }
     } else {
@@ -789,10 +718,16 @@ const loadOperatorKeysFromRPC = async (
 
     const keysOfOwnersCount = Object.keys(sentryKeysMap).length;
     // Map the keys from pools our operator should operate, filter pools if passedInOwnersAndPools whitelist is enabled
-    sentryKeysMap = await reloadPoolKeysForRPC(operator, sentryKeysMap, passedInOwnersAndPools);
+    const ownedPools = await reloadPoolKeysForRPC(operator, passedInOwnersAndPools);
 
     // Map all the assigned pools from keys of owners
-    sentryKeysMap = await syncOwnerStakedKeysForRPC(owners, sentryKeysMap);
+    const userInteractedPools = await getMultipleUsersInteractedPoolsRpc(owners);
+
+    // Merge ownedPools and userInteractedPools without duplicates
+    const uniquePools = new Set<string>([...ownedPools, ...userInteractedPools]);
+
+    // Assign the merged, deduplicated pools to cachedPoolsOfOperator
+    cachedPoolsOfOperator = Array.from(uniquePools);
 
     // Sync the nodeLicenseStatusMap
     Object.keys(sentryKeysMap).forEach(key => {
