b3d44e299aba92fb74e83601445d62d2347c447b
Stanyslav P <sp@cryptit.at>
2024-07-19 16:08:44 +0300

    improved web-staking tailwind config

--- apps/web-staking/tailwind.config.ts
+++ apps/web-staking/tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.tsx",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
