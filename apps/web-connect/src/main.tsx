33c00b3a616cb506ba8b2c7b88d331073750cd9b
CryptIT Austria <office@cryptit.at>
2024-08-09 15:52:25 +0200

    add changes to web3modal post merge

--- apps/web-connect/src/main.tsx
+++ apps/web-connect/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {AppRoutes} from './features/router'
-import { WagmiProvider, createConfig, http  } from 'wagmi'
-import {Chain} from 'viem'
+import { AppRoutes } from './features/router'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { Config, WagmiProvider } from 'wagmi'
+import { Chain } from 'viem'
 import { createWeb3Modal } from "@web3modal/wagmi/react"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {arbitrum, arbitrumSepolia} from 'wagmi/chains'
+import { arbitrum } from 'wagmi/chains'
 import './index.css'
 import { IpLocationChecker } from './features/ipchecker/IpLocationChecker'
 
@@ -13,7 +14,7 @@ const projectId = '8f5121741edc292ac7e4203b648d61e2'
 
 export const chains: [Chain, ...Chain[]] = [
 	arbitrum as Chain,
-	arbitrumSepolia as Chain,
+	// arbitrumSepolia as Chain
 ]
 
 const queryClient = new QueryClient()
@@ -21,30 +22,40 @@ const queryClient = new QueryClient()
 const metadata = {
 	name: 'Xai Sentry Node',
 	description: 'Connect your wallet to the Xai Sentry Node',
-	url: 'https://web3modal.com',
-	icons: ['https://avatars.githubusercontent.com/u/37784886']
+    url: 'https://app.xai.games/',
+    icons: ['https://xai.games/images/delta%20med.svg']
 }
 
-export const wagmiConfig = createConfig({
-	chains,	
-	transports: {
-		[arbitrum.id]: http(),
-		[arbitrumSepolia.id]: http(),
-	},
+export const wagmiConfig = defaultWagmiConfig({
+	chains, // required
+	projectId, // required
+	metadata, // required
+	ssr: true,
+	// storage: createStorage({
+	//     storage: cookieStorage
+	// }),
+	// transports: {
+	// 	[arbitrum.id]: http(),
+	// 	[arbitrumSepolia.id]: http(),
+	// },
+	enableWalletConnect: true, // Optional - true by default
+	enableInjected: true, // Optional - true by default
+	enableEIP6963: true, // Optional - true by default
+	enableCoinbase: true, // Optional - true by default
+	// ...wagmiOptions // Optional - Override createConfig parameters
 })
 
 createWeb3Modal({
 	projectId,
-	wagmiConfig,
-	metadata,
+	wagmiConfig
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<WagmiProvider  config={wagmiConfig as any}>
+	<WagmiProvider config={wagmiConfig as Config}>
 		<QueryClientProvider client={queryClient}>
 			<React.StrictMode>
 				<IpLocationChecker>
-					<AppRoutes/>
+					<AppRoutes />
 				</IpLocationChecker>
 			</React.StrictMode>
 		</QueryClientProvider>
