6e8a9ecb81f3608702a37c490b8b34a5555a9650
Jerry Hodges <jerryhodges@expopulus.com>
2024-08-12 18:05:01 -0400

    Update Operator Logic for V1 & V2 Functions

--- packages/core/src/operator/operator-runtime/listenForChallengesCallback.ts
+++ packages/core/src/operator/operator-runtime/listenForChallengesCallback.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
-import { PublicNodeBucketInformation } from "../index.js";
+import { checkRefereeBulkSubmissionCompatible, PublicNodeBucketInformation } from "../index.js";
 import { operatorState } from "./operatorState.js";
-import { Challenge, config, getSubgraphHealthStatus } from "../../index.js";
+import { Challenge, config, getSentryWalletsForOperator, getSubgraphHealthStatus, retry } from "../../index.js";
 import { processNewChallenge_V1 } from "./operator-v1/processNewChallenge.js";
 import { loadOperatorKeysFromGraph_V1 } from "./operator-v1/loadOperatorKeysFromGraph.js";
 import { loadOperatorKeysFromRPC_V1 } from "./operator-v1/loadOperatorKeysFromRPC.js";
 import { processClosedChallenges_V1 } from "./operator-v1/processClosedChallenges.js";
+import { loadOperatorWalletsFromGraph } from "./loadOperatorWalletsFromGraph.js";
+import { processNewChallenge } from "./processNewChallenge.js";
+import { processClosedChallenge } from "./processClosedChallenge.js";
+import { loadOperatorWalletsFromRPC } from "./loadOperatorWalletsFromRPC.js";
 
 /**
  * Update the status message for display in the operator desktop app key list
@@ -41,22 +45,61 @@ export async function listenForChallengesCallback(challengeNumber: bigint, chall
     try {
         const graphStatus = await getSubgraphHealthStatus();
         if (graphStatus.healthy) {
-                const { sentryWalletMap, sentryKeysMap, nodeLicenseIds, mappedPools, refereeConfig } = await loadOperatorKeysFromGraph_V1(operatorState.operatorAddress, challengeNumber - 1n);
+            // Load all operator addresses and pool addresses, pass in the whitelist to get them filtered
+            // refereeConfig will be used to locally calculate the boos factor (hold tier thresholds and boostFactors as well as max staking capacity per key)
+            const { wallets, pools, refereeConfig: refereeConfigFromGraph } = await retry(() => getSentryWalletsForOperator(operatorState.operatorAddress, operatorState.passedInOwnersAndPools));
+
+            // Check if the referee has been upgraded to V2
+            const refereeIsV2 = await checkRefereeBulkSubmissionCompatible(refereeConfigFromGraph);
+
+            // If the referee has been upgraded to V2, use the V2 functions
+            if (refereeIsV2) {
+                const sentryWalletData = {
+                    wallets,
+                    pools
+                }
+                const walletsAndPools = await loadOperatorWalletsFromGraph(operatorState.operatorAddress, sentryWalletData, challengeNumber - 1n);
+                await processNewChallenge(challengeNumber, challenge, walletsAndPools, refereeConfigFromGraph);
+
+                // Process the previous challenge, that should be closed now
+                if (challengeNumber > BigInt(1)) {
+                    await processClosedChallenge(challengeNumber - BigInt(1), walletsAndPools);
+                }
+
+            } else {
+                const { sentryWalletMap, sentryKeysMap, nodeLicenseIds, mappedPools, refereeConfig } = await loadOperatorKeysFromGraph_V1(wallets, pools, refereeConfigFromGraph, challengeNumber - 1n);
                 await processNewChallenge_V1(challengeNumber, challenge, nodeLicenseIds, sentryKeysMap, sentryWalletMap, mappedPools, refereeConfig);
                 // check the previous challenge, that should be closed now
                 if (challengeNumber > BigInt(1)) {
                     await processClosedChallenges_V1(challengeNumber - BigInt(1), nodeLicenseIds, sentryKeysMap, sentryWalletMap);
                 }
-
+            }
         } else {
-            operatorState.cachedLogger(`Revert to RPC call instead of using subgraph. Subgraph status error: ${graphStatus.error}`)
+            // If the subgraph is not healthy, use the RPC call to get the operator keys
+            operatorState.cachedLogger(`Revert to RPC call instead of using subgraph. Subgraph status error: ${graphStatus.error}`);
+
+            // Check if the referee has been upgraded to V2
+            const refereeIsV2 = await checkRefereeBulkSubmissionCompatible();
+
+            // If the referee has been upgraded to V2, use the V2 functions
+            if (refereeIsV2) {
+                const bulkWalletsAndPools = await loadOperatorWalletsFromRPC(operatorState.operatorAddress);
+                await processNewChallenge(challengeNumber, challenge, bulkWalletsAndPools);
+                // Process the previous challenge, that should be closed now
+                if (challengeNumber > BigInt(1)) {
+                    await processClosedChallenge(challengeNumber - BigInt(1), bulkWalletsAndPools);
+                }
+            } else {
+                
+                // If the referee has not been upgraded to V2, use the V1 functions
                 const { sentryKeysMap, nodeLicenseIds } = await loadOperatorKeysFromRPC_V1(operatorState.operatorAddress);
-    
                 await processNewChallenge_V1(challengeNumber, challenge, nodeLicenseIds, sentryKeysMap);
+                
                 // check the previous challenge, that should be closed now
                 if (challengeNumber > BigInt(1)) {
                     await processClosedChallenges_V1(challengeNumber - BigInt(1), nodeLicenseIds, sentryKeysMap);
                 }
+            }
         }
     } catch (error: any) {
         operatorState.cachedLogger(`Error processing new challenge in listener callback: - ${error && error.message ? error.message : error}`);
--- packages/core/src/operator/operator-runtime/operator-v1/loadOperatorKeysFromGraph.ts
+++ packages/core/src/operator/operator-runtime/operator-v1/loadOperatorKeysFromGraph.ts
@@ -13,7 +13,9 @@ import { getPoolInfosFromGraph, getSentryKeysFromGraph, getSentryWalletsForOpera
  * @returns mapped sentry key objects, pool objects, owner information and Referee config (this will be used to locally calculate the boost Factor)
  */
 export const loadOperatorKeysFromGraph_V1 = async (
-    operator: string,
+    wallets: SentryWallet[],
+    pools: PoolInfo[],
+    refereeConfig: RefereeConfig,
     latestChallengeNumber?: bigint
 ): Promise<{
     wallets: SentryWallet[],
@@ -30,10 +32,6 @@ export const loadOperatorKeysFromGraph_V1 = async (
     } else {
         operatorState.cachedLogger(`No operator owners were passed in.`);
     }
-
-    // Load all operator addresses and pool addresses, pass in the whitelist to get them filtered
-    // refereeConfig will be used to locally calculate the boos factor (hold tier thresholds and boostFactors as well as max staking capacity per key)
-    const { wallets, pools, refereeConfig } = await retry(() => getSentryWalletsForOperator(operator, operatorState.passedInOwnersAndPools));
 
     if (wallets.length == 0 && pools.length == 0) {
         operatorState.cachedLogger(`No operatorWallets found, approve your wallet for operating keys or delegate it to a staking pool to operate for it.`);
