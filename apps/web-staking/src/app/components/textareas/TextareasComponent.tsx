1cd4bce0373820ac25406dcece7e07e7255016a3
CryptIT Austria <office@cryptit.at>
2024-08-16 14:59:25 +0200

    remove artifact color, fix warning background on key sale page. Fix textarea on staking (browser update ?)

--- apps/web-staking/src/app/components/textareas/TextareasComponent.tsx
+++ apps/web-staking/src/app/components/textareas/TextareasComponent.tsx
@@ -35,6 +35,7 @@ export const PoolTextarea = ({
         inputWrapper: `relative ${isInvalid ? "bg-bananaBoat" : "bg-greyBorders"} rounded-none global-input-clip-path ${
           isInvalid ? "border-red" : ""
         } p-[1px] ${isInvalid ? "data-[hover=true]:bg-bananaBoat group-data-[focus=true]:bg-bananaBoat" : "data-[hover=true]:bg-hornetSting group-data-[focus=true]:bg-hornetSting"}`,
+        innerWrapper: "!bg-nulnOil",
         label: `text-lg !text-americanSilver font-bold pb-3 `,
         input: "text-lg !text-americanSilver !placeholder-dugong pl-[10px] !bg-nulnOil global-input-clip-path py-2",
         errorMessage: "absolute text-[#ED5F00] text-base font-normal",
