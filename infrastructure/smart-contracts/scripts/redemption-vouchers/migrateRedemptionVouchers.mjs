67992d551983c3dea73402763cefedc8b5a6ab1e
Jerry Hodges <jerryhodges@expopulus.com>
2024-08-24 00:23:12 -0400

    Checkpoint

---
+++ infrastructure/smart-contracts/scripts/redemption-vouchers/migrateRedemptionVouchers.mjs
@@ -0,0 +1,30 @@
+import { config } from "@sentry/core";
+
+async function main() {
+
+    // get the deployer
+    const [deployer] = (await ethers.getSigners());
+    const deployerAddress = await deployer.getAddress();
+    console.log("deployerAddress", deployerAddress);
+
+    // deploy counter contract
+    console.log("Starting redemption migration...");
+    const esXai = await ethers.getContractFactory("esXai4");
+    const esXaiInstance = await new ethers.Contract(config.esXaiAddress, esXai.interface, deployer);
+
+    // Step 1: Determine How Many Redemptions Can be Processed in a Single Transaction
+    // Step 2: Retrieve a large block of redemption vouchers from the subgraph
+    // Step 3: Process the redemption vouchers in batches of the maximum size
+    // Step 4: Wait for the transaction to be mined
+    // Step 5: Repeat steps 2-4 until all redemption vouchers have been processed
+
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
