07bd947f3084b13d53f62284689578319b891d01
Jerry Hodges <jerryhodges@expopulus.com>
2024-08-25 14:21:38 -0400

    Formatting

--- infrastructure/sentry-subgraph/src/esXai.ts
+++ infrastructure/sentry-subgraph/src/esXai.ts
@@ -1,5 +1,5 @@
-import {  BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts"
-import { 
+import { BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts"
+import {
     RedemptionStarted as RedemptionStartedEvent,
     RedemptionCancelled as RedemptionCancelledEvent,
     RedemptionCompleted as RedemptionCompletedEvent,
@@ -11,16 +11,16 @@ import { RedemptionRequest, SentryWallet } from "../generated/schema";
 export function handleRedemptionStarted(event: RedemptionStartedEvent): void {
 
     // Create a new redemption request entity 
-    const request = new RedemptionRequest(event.params.user.toHexString() + "_"  + event.params.index.toString());
-    
+    const request = new RedemptionRequest(event.params.user.toHexString() + "_" + event.params.index.toString());
+
     // Decode the transaction input data to get the redemption amount and duration
     const inputData = event.transaction.input.toHexString().slice(10); // Remove function selector
     const decoded = ethereum.decode('(uint256,uint256)', Bytes.fromHexString(inputData) as Bytes);
 
-    if (decoded) {      
+    if (decoded) {
         const decodedTuple = decoded.toTuple();
-            request.amount = decodedTuple[0].toBigInt();
-            request.duration = decodedTuple[1].toBigInt();
+        request.amount = decodedTuple[0].toBigInt();
+        request.duration = decodedTuple[1].toBigInt();
     } else {
         log.warning("Failed to decode startRedemption TX: {}", [event.transaction.hash.toHexString()]);
         return;
@@ -37,9 +37,9 @@ export function handleRedemptionStarted(event: RedemptionStartedEvent): void {
 }
 
 export function handleRedemptionCancelled(event: RedemptionCancelledEvent): void {
-    const request = RedemptionRequest.load(event.params.user.toHexString() + "_"  + event.params.index.toString());
+    const request = RedemptionRequest.load(event.params.user.toHexString() + "_" + event.params.index.toString());
 
-    if(!request) {
+    if (!request) {
         log.warning("Failed to find redemption request on handleRedemptionCancelled: TX: " + event.transaction.hash.toHexString() + ", user: " + event.params.user.toHexString() + ", index: " + event.params.index.toString(), []);
         return;
     }
@@ -48,19 +48,19 @@ export function handleRedemptionCancelled(event: RedemptionCancelledEvent): void
     request.endTime = event.block.timestamp;
     request.completed = true;
     request.save();
-    
+
 }
 export function handleRedemptionCompleted(event: RedemptionCompletedEvent): void {
-    const request = RedemptionRequest.load(event.params.user.toHexString() + "_"  + event.params.index.toString());
+    const request = RedemptionRequest.load(event.params.user.toHexString() + "_" + event.params.index.toString());
 
-    if(!request) {
+    if (!request) {
         log.warning("Failed to find redemption request on handleRedemptionCompleted: TX: " + event.transaction.hash.toHexString() + ", user: " + event.params.user.toHexString() + ", index: " + event.params.index.toString(), []);
         return;
     }
 
     request.endTime = event.block.timestamp;
     request.completed = true;
-    request.save();    
+    request.save();
 }
 
 export function handleVoucherIssued(event: VoucherIssuedEvent): void {
@@ -68,14 +68,14 @@ export function handleVoucherIssued(event: VoucherIssuedEvent): void {
     // Get the user and  indices of the vouchers issued
     const user = event.params.user.toHexString();
     const indices = event.params.indices;
-    
+
     // Loop through the indices and update the voucherIssued field on the redemption request
     for (let i = 0; i < indices.length; i++) {
 
         // Load the existing redemption request from the store
-        const request = RedemptionRequest.load(user + "_"  + indices[i].toString());
+        const request = RedemptionRequest.load(user + "_" + indices[i].toString());
 
-        if(!request) {
+        if (!request) {
             log.warning("Failed to find redemption request on handleVoucherIssued: TX: " + event.transaction.hash.toHexString() + ", user: " + user + ", index: " + indices[i].toString(), []);
             continue;
         }
@@ -87,7 +87,7 @@ export function handleVoucherIssued(event: VoucherIssuedEvent): void {
 
 }
 
-export function handleesXaiTransfer(event:esXaiTransferEvent): void {
+export function handleesXaiTransfer(event: esXaiTransferEvent): void {
     // Load the receiving wallet
     let receivingSentryWallet = SentryWallet.load(event.params.to.toHexString())
 
@@ -108,7 +108,7 @@ export function handleesXaiTransfer(event:esXaiTransferEvent): void {
     receivingSentryWallet.save();
 
     // If the sender is the zero address, this is a mint operation, so return    
-    if(event.params.from.toHexString() == "0x0000000000000000000000000000000000000000") return;
+    if (event.params.from.toHexString() == "0x0000000000000000000000000000000000000000") return;
 
     // Load the sending wallet    
     const sendingSentryWallet = SentryWallet.load(event.params.from.toHexString())
@@ -118,9 +118,9 @@ export function handleesXaiTransfer(event:esXaiTransferEvent): void {
     if (!sendingSentryWallet) {
         log.warning("Failed to find sending wallet on handleesXaiTransfer: TX: " + event.transaction.hash.toHexString() + ", user: " + event.params.from.toHexString(), []);
         return;
-    }else{
-    // Update the sending wallet esXai balance
-    sendingSentryWallet.esXaiBalance = sendingSentryWallet.esXaiBalance.minus(event.params.value)
-    sendingSentryWallet.save();
+    } else {
+        // Update the sending wallet esXai balance
+        sendingSentryWallet.esXaiBalance = sendingSentryWallet.esXaiBalance.minus(event.params.value)
+        sendingSentryWallet.save();
     }
 }
