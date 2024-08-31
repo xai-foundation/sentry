e549108c75878a1c0763a7d3ba6cb31c9f61fc26
Jerry Hodges <jerryhodges@expopulus.com>
2024-08-31 16:21:33 -0400

    Formatting

--- packages/core/src/data-centralization/dataCentralizationRuntime.ts
+++ packages/core/src/data-centralization/dataCentralizationRuntime.ts
@@ -36,135 +36,135 @@ const toSaveString = (obj: any) => {
  * @returns {Promise<() => Promise<void>>} A function to stop the runtime.
  */
 export async function dataCentralizationRuntime({
-    mongoUri,
-    slackWebHookUrl,
-    logFunction = (_) => {}
+	mongoUri,
+	slackWebHookUrl,
+	logFunction = (_) => { }
 }: DataCentralizationRuntimeArgs): Promise<() => Promise<void>> {
 
-    // Establish a connection to MongoDB via mongoose.
-    try {
-        await mongoose.connect(mongoUri);
-        logFunction(`Connected to MongoDB`);
-    } catch (error) {
-        logFunction(`Failed to connect to MongoDB: ${error}`);
-        throw new Error(`Failed to connect to MongoDB: ${error}`);
-    }
+	// Establish a connection to MongoDB via mongoose.
+	try {
+		await mongoose.connect(mongoUri);
+		logFunction(`Connected to MongoDB`);
+	} catch (error) {
+		logFunction(`Failed to connect to MongoDB: ${error}`);
+		throw new Error(`Failed to connect to MongoDB: ${error}`);
+	}
 
-    // Establish a single event listener for multiple events for data centralization.
-    // Map poolfactory event to index of pool address in event logs
-    const eventToPoolAddressInLog: { [eventName: string]: number } = {
-        'PoolCreated': 1,
-        'StakeEsXai': 1,
-        'UnstakeEsXai': 1,
-        'StakeKeys': 1,
-        'UnstakeKeys': 1,
-        'UpdatePoolDelegate': 1,
-        'UnstakeRequestStarted': 1,
-        'UpdateShares': 0,
-        'UpdateMetadata': 0
-    }
+	// Establish a single event listener for multiple events for data centralization.
+	// Map poolfactory event to index of pool address in event logs
+	const eventToPoolAddressInLog: { [eventName: string]: number } = {
+		'PoolCreated': 1,
+		'StakeEsXai': 1,
+		'UnstakeEsXai': 1,
+		'StakeKeys': 1,
+		'UnstakeKeys': 1,
+		'UpdatePoolDelegate': 1,
+		'UnstakeRequestStarted': 1,
+		'UpdateShares': 0,
+		'UpdateMetadata': 0
+	}
 
-    const stopListener = resilientEventListener({
-        rpcUrl: config.arbitrumOneWebSocketUrl,
-        contractAddress: config.poolFactoryAddress,
-        abi: PoolFactoryAbi,
-        eventName: Object.keys(eventToPoolAddressInLog),
-        log: logFunction,
-        callback: (log: LogDescription | null, err?: EventListenerError) => {
-            if (err) {
-                logFunction(`Error listening to event: ${err.message}`);
-            } else if (log) {
-                logFunction(`Event ${log.name} received: ${toSaveString(log.args)}`);
+	const stopListener = resilientEventListener({
+		rpcUrl: config.arbitrumOneWebSocketUrl,
+		contractAddress: config.poolFactoryAddress,
+		abi: PoolFactoryAbi,
+		eventName: Object.keys(eventToPoolAddressInLog),
+		log: logFunction,
+		callback: (log: LogDescription | null, err?: EventListenerError) => {
+			if (err) {
+				logFunction(`Error listening to event: ${err.message}`);
+			} else if (log) {
+				logFunction(`Event ${log.name} received: ${toSaveString(log.args)}`);
 
-                const poolAddress = log.args[eventToPoolAddressInLog[log.name]];
-                if (!poolAddress) {
-                    logFunction(`Error finding poolAddress on event ${log.name}`);
-                    return;
-                }
+				const poolAddress = log.args[eventToPoolAddressInLog[log.name]];
+				if (!poolAddress) {
+					logFunction(`Error finding poolAddress on event ${log.name}`);
+					return;
+				}
 
-                retry(() => updatePoolInDB(poolAddress, log.name), 3)
-                    .then(() => {
-                        logFunction("Updated pool:" + poolAddress)
-                    })
-                    .catch(err => {
-                        logFunction(`Error updating pool ${poolAddress} on event ${log.name}, error: ${err}`);
-                    });
+				retry(() => updatePoolInDB(poolAddress, log.name), 3)
+					.then(() => {
+						logFunction("Updated pool:" + poolAddress)
+					})
+					.catch(err => {
+						logFunction(`Error updating pool ${poolAddress} on event ${log.name}, error: ${err}`);
+					});
 
-            } else {
-                logFunction(`Received null log description.`);
-            }
-        },
-    }).stop;
+			} else {
+				logFunction(`Received null log description.`);
+			}
+		},
+	}).stop;
 
-    const closeChallengeListener = listenForChallenges(
-        async (challengeNumber: bigint, challenge: Challenge, event?: any) => {
-            try {
-                const startTime = new Date().getTime();
-                const PoolModel = mongoose.models.Pool || mongoose.model<IPool>('Pool', PoolSchema);
+	const closeChallengeListener = listenForChallenges(
+		async (challengeNumber: bigint, challenge: Challenge, event?: any) => {
+			try {
+				const startTime = new Date().getTime();
+				const PoolModel = mongoose.models.Pool || mongoose.model<IPool>('Pool', PoolSchema);
 
-                const slackStartMessage = `Starting pool sync update for challenge ${challengeNumber}`;
-                await sendSlackNotification(slackWebHookUrl, slackStartMessage, logFunction);
+				const slackStartMessage = `Starting pool sync update for challenge ${challengeNumber}`;
+				await sendSlackNotification(slackWebHookUrl, slackStartMessage, logFunction);
 
-                const graphUpdateStartTime = new Date().getTime();
+				const graphUpdateStartTime = new Date().getTime();
 
-                const updatedPools = await retry(() => getRewardRatesFromGraph([]));
+				const updatedPools = await retry(() => getRewardRatesFromGraph([]));
 
-                const graphUpdateEndTime = new Date().getTime();
+				const graphUpdateEndTime = new Date().getTime();
 
-                const mongoInsertStartTime = new Date().getTime();
+				const mongoInsertStartTime = new Date().getTime();
 
-                for (const updatedPool of updatedPools) {
-                    const checksumAddress = getAddress(updatedPool.poolAddress);
+				for (const updatedPool of updatedPools) {
+					const checksumAddress = getAddress(updatedPool.poolAddress);
 
-                    await PoolModel.updateOne(
-                        { poolAddress: checksumAddress },
-                        {
-                            $set: {
-                                esXaiRewardRate: updatedPool.averageDailyEsXaiReward,
-                                keyRewardRate: updatedPool.averageDailyKeyReward,
-                                totalEsXaiClaimed: updatedPool.totalEsXaiClaimed
-                            }
-                        },
-                    );
-                }
-                const mongoInsertEndTime = new Date().getTime();
-                const totalSeconds = mongoInsertEndTime - startTime;
-                const totalGraphSeconds = graphUpdateEndTime - graphUpdateStartTime;
-                const totalMongoSeconds = mongoInsertEndTime - mongoInsertStartTime;
-                const slackMessage = `Finished pool sync update for ${updatedPools.length} pools in ${totalSeconds}ms. Graph update took ${totalGraphSeconds}ms. Mongo insert took ${totalMongoSeconds}ms.`;
+					await PoolModel.updateOne(
+						{ poolAddress: checksumAddress },
+						{
+							$set: {
+								esXaiRewardRate: updatedPool.averageDailyEsXaiReward,
+								keyRewardRate: updatedPool.averageDailyKeyReward,
+								totalEsXaiClaimed: updatedPool.totalEsXaiClaimed
+							}
+						},
+					);
+				}
+				const mongoInsertEndTime = new Date().getTime();
+				const totalSeconds = mongoInsertEndTime - startTime;
+				const totalGraphSeconds = graphUpdateEndTime - graphUpdateStartTime;
+				const totalMongoSeconds = mongoInsertEndTime - mongoInsertStartTime;
+				const slackMessage = `Finished pool sync update for ${updatedPools.length} pools in ${totalSeconds}ms. Graph update took ${totalGraphSeconds}ms. Mongo insert took ${totalMongoSeconds}ms.`;
 
-                await sendSlackNotification(slackWebHookUrl, slackMessage, logFunction);
-            } catch (error) {
-                const errorMessage = error instanceof Error ? error.message : String(error);
-                await sendSlackNotification(slackWebHookUrl, `Error in challenge listener: ${errorMessage}`, logFunction);
-                logFunction(`Error in challenge listener: ${errorMessage}`);
-                // Log the error but do not re-throw here
-            }
-        },
-        async (error: Error) => {
-            const errorMessage = `Error in listenForChallenges: ${error.message}`;
-            await sendSlackNotification(slackWebHookUrl, errorMessage, logFunction);
-            logFunction(errorMessage);
-            // Log the error but do not re-throw here
-        }
-    );
+				await sendSlackNotification(slackWebHookUrl, slackMessage, logFunction);
+			} catch (error) {
+				const errorMessage = error instanceof Error ? error.message : String(error);
+				await sendSlackNotification(slackWebHookUrl, `Error in challenge listener: ${errorMessage}`, logFunction);
+				logFunction(`Error in challenge listener: ${errorMessage}`);
+				// Log the error but do not re-throw here
+			}
+		},
+		async (error: Error) => {
+			const errorMessage = `Error in listenForChallenges: ${error.message}`;
+			await sendSlackNotification(slackWebHookUrl, errorMessage, logFunction);
+			logFunction(errorMessage);
+			// Log the error but do not re-throw here
+		}
+	);
 
-    /**
-     * Stops the data centralization runtime.
-     * @returns {Promise<void>} A promise that resolves when the runtime is successfully stopped.
-     */
-    return async () => {
-        try {
-            // Disconnect from MongoDB.
-            await mongoose.disconnect();
-            logFunction('Disconnected from MongoDB.');
-            closeChallengeListener();
-            // Remove event listener listener.
-            stopListener();
-            logFunction('Event listener removed.');
-        } catch (error) {
-            logFunction(`Error stopping the data centralization runtime: ${error}`);
-            throw error; // This re-throws the error to ensure it is handled by the caller.
-        }
-    };
+	/**
+	 * Stops the data centralization runtime.
+	 * @returns {Promise<void>} A promise that resolves when the runtime is successfully stopped.
+	 */
+	return async () => {
+		try {
+			// Disconnect from MongoDB.
+			await mongoose.disconnect();
+			logFunction('Disconnected from MongoDB.');
+			closeChallengeListener();
+			// Remove event listener listener.
+			stopListener();
+			logFunction('Event listener removed.');
+		} catch (error) {
+			logFunction(`Error stopping the data centralization runtime: ${error}`);
+			throw error; // This re-throws the error to ensure it is handled by the caller.
+		}
+	};
 }
