0b5d397ea409599729629c077aedaed8dec7a870
Jerry Hodges <jerryhodges@expopulus.com>
2024-09-19 12:21:52 -0400

    Remove try catch

--- packages/core/src/operator/getChallengerPublicKey.ts
+++ packages/core/src/operator/getChallengerPublicKey.ts
@@ -5,16 +5,9 @@ import { getProvider, RefereeAbi } from "../index.js";
 
 
 export async function getChallengerPublicKey(): Promise<string> {
+    const provider = getProvider();
+    const referee = new ethers.Contract(config.refereeAddress, RefereeAbi, provider);
+    const publicKey = await referee.challengerPublicKey();
 
-    try {
-        const provider = getProvider();
-        const referee = new ethers.Contract(config.refereeAddress, RefereeAbi, provider);
-        const publicKey = await referee.challengerPublicKey();
-
-        return publicKey;
-        
-    } catch (error: unknown) {
-        const message = error instanceof Error ? error.message : 'An unknown error occurred';
-        throw new Error(`Error getting challenger public key: ${message}`);        
-    }
+    return publicKey;
 }
