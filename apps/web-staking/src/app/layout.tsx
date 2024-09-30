8a07b3d16302066d8636c412773ff5852259afb6
Jerry Hodges <jerryhodges@expopulus.com>
2024-09-30 16:06:07 -0400

    feat: Added OG & Twitter Social Sharing Card/Tags

--- apps/web-staking/src/app/layout.tsx
+++ apps/web-staking/src/app/layout.tsx
@@ -29,6 +29,19 @@ export default function xRootLayout({
   // const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
+      <head>
+        <meta name="title" property="og:title" content="Xai Staking App"/>
+        <meta name="description" property="og:description" content="Stake keys & esXai to earn rewards"/>
+        <meta name="image" property="og:image" content="/images/sentry-share.jpg"/>
+        <meta name="url" property="og:url" content="https://app.xai.games"/>
+        <meta name="type" property="og:type" content="website"/>    
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@xai_games"/>
+        <meta name="twitter:title" content="Xai Staking App"/>
+        <meta name="twitter:description" content="Stake keys & esXai to earn rewards"/>
+        <meta name="twitter:image" content="/images/sentry-share.jpg"/>
+        <meta name="twitter:creator" content="@xai_games"/>
+      </head>
       <body className="bg-background-image overflow-y-scroll bg-cover bg-fixed bg-center bg-no-repeat">
         <ContextProvider>
             <IpLocationChecker>
