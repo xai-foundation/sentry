0f6faa1a76b6bad4b85a725408309f196e7ec32a
Jerry Hodges <jerryhodges@expopulus.com>
2024-10-01 14:16:05 -0400

    feat: Updated test cases to test using mintTo function with a promo code

--- infrastructure/smart-contracts/test/NodeLicenseTinyKeys.mjs
+++ infrastructure/smart-contracts/test/NodeLicenseTinyKeys.mjs
@@ -460,5 +460,36 @@ export function NodeLicenseTinyKeysTest(deployInfrastructure, poolConfigurations
             const totalSupplyAfterMint = await nodeLicense.totalSupply();
             expect(totalSupplyAfterMint).to.eq(totalSupplyBeforeMint + BigInt(1));
         });
+        
+        it("Check minting with a promo code using the mintTo function and receiving the correct funds", async function() {
+            const {nodeLicense, nodeLicenseDefaultAdmin, addr1: referrer, addr2: minter, addr3: keyReceiver, fundsReceiver} = await loadFixture(deployInfrastructure);
+            const promoCode = "PROMO2023";
+            const initialBalance = await ethers.provider.getBalance(fundsReceiver.address);
+            const initialKeyBalance = await nodeLicense.balanceOf(keyReceiver.address);
+            const referralRewardPercentage = await nodeLicense.referralRewardPercentage();
+
+            // Create a new promo code
+            await nodeLicense.connect(nodeLicenseDefaultAdmin).createPromoCode(promoCode, referrer.address);
+
+            // Mint an key with a promo code
+            const price = await nodeLicense.price(1, promoCode);
+            const referralReward = price * referralRewardPercentage / BigInt(100);
+
+            await nodeLicense.connect(minter).mintTo(keyReceiver.address, 1, promoCode, { value: price });
+
+            // Check the key was received
+            const keyBalanceAfter = await nodeLicense.balanceOf(keyReceiver.address);
+            expect(keyBalanceAfter).to.eq(initialKeyBalance + BigInt(1));
+
+            // Check the fundsReceiver received the correct funds
+            const finalBalance = await ethers.provider.getBalance(fundsReceiver.address);
+            expect(finalBalance).to.eq(initialBalance + price - referralReward);
+
+            // Check the contract balance is the referral reward
+            const contractBalance = await ethers.provider.getBalance(await nodeLicense.getAddress());
+            expect(contractBalance).to.eq(referralReward);
+
+        });
+
     }
 }
