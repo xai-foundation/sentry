b179beecde6b7e91f624c2efa71105abe73526cb
Jerry Hodges <jerryhodges@expopulus.com>
2024-10-10 18:45:38 -0400

    feat: Added tests for close challenge function

--- infrastructure/smart-contracts/test/Referee.mjs
+++ infrastructure/smart-contracts/test/Referee.mjs
@@ -737,6 +737,10 @@ export function RefereeTests(deployInfrastructure) {
 			const challenge = await referee.getChallenge(challengeCounter);
 			expect(challenge.openForSubmissions).to.be.eq(true);									// Confirm the challenge exists and is open
 
+			const fiftyFiveMinutes = 60 * 55;
+            await network.provider.send("evm_increaseTime", [fiftyFiveMinutes]);
+            await network.provider.send("evm_mine");          
+
 			await referee.connect(refereeDefaultAdmin).closeCurrentChallenge();						// Close the challenge
 
 			const challengeAfterClose = await referee.getChallenge(challengeCounter);
@@ -747,7 +751,24 @@ export function RefereeTests(deployInfrastructure) {
 			const newChallenge = await referee.getChallenge(challengeCounter + 1n);
 			
 			expect(newChallenge.openForSubmissions).to.be.eq(true);									// Confirm the new challenge exists and is open
+		});
+		it("Check that a challenge cannot be closed before 50 minutes have passed.", async function () {
+			const {referee, challenger, refereeDefaultAdmin} = await loadFixture(deployInfrastructure);
+
+			const challengeCounter = await referee.challengeCounter();		
 			
+			const stateRoot = "0x0000000000000000000000000000000000000000000000000000000000000000";
+            const startingAssertion = 100;
+            await submitTestChallenge(referee, challenger, startingAssertion, stateRoot);			// Submit First challenge
+			
+			const challenge = await referee.getChallenge(challengeCounter);
+			expect(challenge.openForSubmissions).to.be.eq(true);									// Confirm the challenge exists and is open
+
+			const thirtyMinutes = 60 * 30;
+            await network.provider.send("evm_increaseTime", [thirtyMinutes]);
+            await network.provider.send("evm_mine");          
+
+			await expect(referee.connect(refereeDefaultAdmin).closeCurrentChallenge()).to.be.revertedWith("59");						
 		});
 
 		it("Check that trying to close a challenge without default admin role will fail.", async function () {
