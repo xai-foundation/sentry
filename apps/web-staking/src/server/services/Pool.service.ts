737b940b597050df5fb5320a6ae7650497d9b5bf
CryptIT Austria <office@cryptit.at>
2024-10-21 18:48:17 +0200

    fix(web-staking): Update for consistent db pool order on sort

--- apps/web-staking/src/server/services/Pool.service.ts
+++ apps/web-staking/src/server/services/Pool.service.ts
@@ -31,18 +31,18 @@ type Pagination = {
 export type PagedPools = { count: number, poolInfos: PoolInfo[], totalPoolsInDB: number }
 
 export const findPools = async ({
-									pagination = {
-										limit: 10,
-										page: 1,
-										sort: [['tierIndex', -1], ['name', 1]],
-									},
-									hideFullEsXai = false,
-									hideFullKeys = false,
-									searchName,
-									owner,
-									network,
-									esXaiMinStake
-								}: {
+	pagination = {
+		limit: 10,
+		page: 1,
+		sort: [['tierIndex', -1], ['name', 1]],
+	},
+	hideFullEsXai = false,
+	hideFullKeys = false,
+	searchName,
+	owner,
+	network,
+	esXaiMinStake
+}: {
 	pagination: Pagination;
 	searchName?: string;
 	owner?: string;
@@ -76,10 +76,14 @@ export const findPools = async ({
 			filter.$expr = { $gt: [maxKeyCount, "$keyCount"] };
 		}
 	}
-	
+
 	// Filter by minimum esXAI stake
-	if(esXaiMinStake){
+	if (esXaiMinStake) {
 		filter.totalStakedAmount = { $gte: esXaiMinStake };
+	}
+
+	if (pagination.sort.length == 1) {
+		pagination.sort.push(['name', 1])
 	}
 
 	try {
