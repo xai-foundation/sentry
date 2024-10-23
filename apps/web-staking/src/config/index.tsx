e3089911671814bbc956f3408411170d7fe05a47
Jerry Hodges <jerryhodges@expopulus.com>
2024-10-23 17:38:26 -0400

    Updated ReOwn Project Ids for Production/Development

--- apps/web-staking/src/config/index.tsx
+++ apps/web-staking/src/config/index.tsx
@@ -5,7 +5,7 @@ import { cookieStorage, createStorage } from '@wagmi/core'
 import { arbitrum, arbitrumSepolia } from '@reown/appkit/networks'
 
 // Get projectId at https://cloud.walletconnect.com
-export const projectId = "aa9e5ff297549e8d0cc518d085c28699";
+export const projectId = process.env.NEXT_PUBLIC_APP_ENV === "development" ? "79e38b4593d43c78d7e9ee38f0cdf4ee" : "aa9e5ff297549e8d0cc518d085c28699";
 
 if (!projectId) throw new Error('Project ID is not defined');
 
