f44d194a4352d932893838c8b82ee21f253aebbc
Jerry Hodges <jerryhodges@expopulus.com>
2024-10-29 17:02:45 -0400

    Added console logs for testing.

--- apps/web-connect/src/HydrationWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState, type ReactNode } from 'react'
-
-export const HydrationWrapper = ({ children }: { children: ReactNode }) => {
-  const [mounted, setMounted] = useState(false)
-  
-  useEffect(() => setMounted(true), [])
-  
-  return mounted ? children : null
-}
