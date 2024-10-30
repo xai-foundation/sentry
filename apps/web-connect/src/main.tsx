0455c20a80322df1f3338f19b8d5901a398c3777
Jerry Hodges <jerryhodges@expopulus.com>
2024-10-29 22:42:23 -0400

    Clean Up Referrer Tags

--- apps/web-connect/src/main.tsx
+++ apps/web-connect/src/main.tsx
@@ -206,9 +206,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <meta name="twitter:description" content="Xai Sentry Node Key Sale Page" />
           <meta name="twitter:image" content={xaiThumbnail} />
           <meta name="twitter:creator" content="@xai_games" />
-          <meta name="referrer" content="strict-origin-when-cross-origin" />
           <meta name="referrer" content="origin" />
-          <meta httpEquiv="referrer-policy" content="origin" />
           <base href={window.location.origin} />
         </Helmet>
         <React.StrictMode>
