f7e9f1c43c65ff936ca88004bd1e64552e35d115
Jerry Hodges <jerryhodges@expopulus.com>
2024-11-01 13:02:13 -0400

    Fix Project Id

--- apps/web-staking/src/config/index.tsx
+++ apps/web-staking/src/config/index.tsx
@@ -3,7 +3,7 @@ import { arbitrumSepolia, arbitrum, AppKitNetwork } from '@reown/appkit/networks
 import { cookieStorage, createStorage } from '@wagmi/core'
 
 // Get projectId at https://cloud.walletconnect.com
-export const projectId = process.env.NEXT_PUBLIC_APP_ENV === "development" ? "7b8edd4521f6f7e5ab27d84d931ddf9b" : "aa9e5ff297549e8d0cc518d085c28699";
+export const projectId = process.env.NEXT_PUBLIC_APP_ENV === "development" ? "79e38b4593d43c78d7e9ee38f0cdf4ee" : "aa9e5ff297549e8d0cc518d085c28699";
 
 if (!projectId) throw new Error('Project ID is not defined');
 
