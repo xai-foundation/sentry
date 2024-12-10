0509a2bb460b28eb7deb7c32b20e34f72052ccef
Jerry Hodges <jerryhodges@expopulus.com>
2024-12-10 16:51:42 -0500

    fix: Formatting

--- apps/web-connect/src/features/checkout/components/crossmint/CrossmintModal.tsx
+++ apps/web-connect/src/features/checkout/components/crossmint/CrossmintModal.tsx
@@ -80,85 +80,84 @@ const CrossmintModal: React.FC<CrossmintModalProps> = ({ isOpen, onClose, totalQ
                     </button>
                 </div>
                 <div className="p-4">
-                    
-                <Suspense fallback={<div>Loading...</div>}>
-                        <div className=" w-full">
-                        <CrossmintEmbeddedCheckout
-                            appearance={{
-                                rules:{
-                                    Label: {
-                                        font:{
-                                            size: styles.fontSizeBase,
-                                            weight: styles.fontWeightPrimary,
+                    <Suspense fallback={<div>Loading...</div>}>
+                            <div className=" w-full">
+                            <CrossmintEmbeddedCheckout
+                                appearance={{
+                                    rules:{
+                                        Label: {
+                                            font:{
+                                                size: styles.fontSizeBase,
+                                                weight: styles.fontWeightPrimary,
+                                            },
+                                            colors:{
+                                                text: styles.colors.textPrimary,
+                                            },
                                         },
-                                        colors:{
-                                            text: styles.colors.textPrimary,
+                                        Input: {
+                                            font:{
+                                                size: styles.fontSizeBase,
+                                                weight: styles.fontWeightPrimary,
+                                            },
+                                            colors:{
+                                                text: styles.colors.textPrimary,
+                                                border: styles.colors.border,
+                                            },
+                                        },
+                                        PrimaryButton: {
+                                            font:{
+                                                size: styles.fontSizeBase,
+                                                weight: styles.fontWeightSecondary,
+                                            },
+                                            colors:{
+                                                background: styles.colors.backgroundTertiary,
+                                                text: styles.colors.textSecondary,
+                                            },
                                         },
                                     },
-                                    Input: {
-                                        font:{
-                                            size: styles.fontSizeBase,
-                                            weight: styles.fontWeightPrimary,
-                                        },
-                                        colors:{
-                                            text: styles.colors.textPrimary,
-                                            border: styles.colors.border,
-                                        },
-                                    },
-                                    PrimaryButton: {
-                                        font:{
-                                            size: styles.fontSizeBase,
-                                            weight: styles.fontWeightSecondary,
-                                        },
-                                        colors:{
-                                            background: styles.colors.backgroundTertiary,
-                                            text: styles.colors.textSecondary,
+                                    variables: {
+                                        colors: {
+                                            backgroundPrimary: styles.colors.background,
+                                            borderPrimary: styles.colors.border,
+                                            warning: styles.colors.danger,
+                                            textPrimary: styles.colors.textPrimary,
+                                            textSecondary: styles.colors.textSecondary,
+                                            danger: styles.colors.danger,
+                                            accent: styles.colors.accent,                                
                                         },
                                     },
+                                }}
+                                recipient={{
+                                    walletAddress: address as `0x${string}`
+                                }}
+                                lineItems={{
+                                    collectionLocator: `crossmint:${collectionId}`,   
+                                    callData: {
+                                        _amount: totalQty,
+                                        _to: address as `0x${string}`,
+                                        _promoCode: promoCode,
+                                        _expectedCostInUSDC: (BigInt(totalPriceInUsdc) / BigInt(10 ** 12)).toString(), // 10^12 to reduce 18 decimals to 6 decimals
+                                        totalPrice: formatWeiToEther(totalPriceInUsdc, 6), // convert to 6 decimal places for Crossmint
                                 },
-                                variables: {
-                                    colors: {
-                                        backgroundPrimary: styles.colors.background,
-                                        borderPrimary: styles.colors.border,
-                                        warning: styles.colors.danger,
-                                        textPrimary: styles.colors.textPrimary,
-                                        textSecondary: styles.colors.textSecondary,
-                                        danger: styles.colors.danger,
-                                        accent: styles.colors.accent,                                
+                                }}
+                                payment={{
+                                    crypto: {
+                                        enabled: true,
+                                        defaultChain: "ethereum",
+                                        defaultCurrency: "eth",                        
                                     },
-                                },
-                            }}
-                            recipient={{
-                                walletAddress: address as `0x${string}`
-                            }}
-                            lineItems={{
-                                collectionLocator: `crossmint:${collectionId}`,   
-                                callData: {
-                                    _amount: totalQty,
-                                    _to: address as `0x${string}`,
-                                    _promoCode: promoCode,
-                                    _expectedCostInUSDC: (BigInt(totalPriceInUsdc) / BigInt(10 ** 12)).toString(), // 10^12 to reduce 18 decimals to 6 decimals
-                                    totalPrice: formatWeiToEther(totalPriceInUsdc, 6), // convert to 6 decimal places for Crossmint
-                            },
-                            }}
-                            payment={{
-                                crypto: {
-                                    enabled: true,
-                                    defaultChain: "ethereum",
-                                    defaultCurrency: "eth",                        
-                                },
-                                fiat: {
-                                    enabled: true,
-                                    allowedMethods: {
-                                        card: true,
-                                        googlePay: true,
+                                    fiat: {
+                                        enabled: true,
+                                        allowedMethods: {
+                                            card: true,
+                                            googlePay: true,
+                                        },
+                                        defaultCurrency: "usd",
                                     },
-                                    defaultCurrency: "usd",
-                                },
-                            }}
-                        />
-                        </div>
-                </Suspense>
+                                }}
+                            />
+                            </div>
+                    </Suspense>
                 </div>
             </div>
         </div>
