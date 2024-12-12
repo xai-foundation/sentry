a019c0e1d357998baa21f8922ae4534467fd0417
Jerry Hodges <jerryhodges@expopulus.com>
2024-12-12 11:07:40 -0500

    fix: Fix broken width on Qty Input

--- apps/web-connect/src/features/checkout/components/ChooseQuantityRow.tsx
+++ apps/web-connect/src/features/checkout/components/ChooseQuantityRow.tsx
@@ -74,7 +74,7 @@ export function ChooseQuantityRow(): JSX.Element {
             </div>
             {/* Quantity input section */}
             <div className="flex w-full justify-end flex-row items-start gap-4 sm:mt-4 lg:mt-10">
-                <div className="flex sm:w-full lg:w-[200px] sm:px-2 lg:px-0 lg:mr-5">
+                <div className="flex sm:w-full lg:w-[200px] sm:px-2 lg:px-0">
                     {/* Custom number input component for selecting quantity */}
                     <XaiNumberInput
                         quantity={quantity}
