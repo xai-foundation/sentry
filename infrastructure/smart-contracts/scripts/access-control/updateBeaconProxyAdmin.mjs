c1801e9f30d0eb33d6421b235711af0789ddf159
Craig <craig.branscom@gmail.com>
2025-01-03 15:32:25 -0500

    Update updateBeaconProxyAdmin.mjs

--- infrastructure/smart-contracts/scripts/access-control/updateBeaconProxyAdmin.mjs
+++ infrastructure/smart-contracts/scripts/access-control/updateBeaconProxyAdmin.mjs
@@ -1,25 +1,25 @@
 import hardhat from "hardhat";
 const { ethers } = hardhat;
-import { config } from "@sentry/core";
 
 // Run with npx hardhat run scripts/access-control/updateBeaconProxyAdmin.mjs --network arbitrumSepolia
 
+//set these variables before running
 const PRIVATE_KEY = "0xPrivateKeyHere";
 const NEW_OWNER_ACCOUNT = "0xAccountAddressHere";
-const POOL_BEACON_ADDRESS = "0xPoolBeaconAddressHere";
+const PROXY_ADMIN_ADDRESS = "0xProxyAdminAddressHere";
 
 async function main() {
     // const [signer] = (await ethers.getSigners());
     const provider = ethers.provider; // Use the provider from Hardhat
     const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
-    // ABI fragment for PoolBeacon
+    // ABI fragment for ProxyAdmin contract
     const abiFragment = [
         "function transferOwnership(address newOwner) public"
     ];
     
-    // Attach to the PoolBeacon contract
-    const contract = new ethers.Contract(POOL_BEACON_ADDRESS, abiFragment, signer);
+    // Attach to the ProxyAdmin contract
+    const contract = new ethers.Contract(PROXY_ADMIN_ADDRESS, abiFragment, signer);
 
     console.log(`Changing ownership...`);
     const tx = await contract.transferOwnership(NEW_OWNER_ACCOUNT);
